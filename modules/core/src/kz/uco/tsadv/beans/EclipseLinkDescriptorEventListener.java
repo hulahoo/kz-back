06d4eed3c5c36d7324daf80a90ffc6de9ae9c12a
Murat Issenzhulov <murat.issenzhulov@uco.kz>
2020-10-28 22:59:01 +0600

    TS-905 If during creation the end date of the AbstractTimeBasedEntity is less than the system one, then instead of the time and author of creation, the time and author of the change are recorded

---
+++ modules/core/src/kz/uco/tsadv/beans/EclipseLinkDescriptorEventListener.java
@@ -0,0 +1,25 @@
+package kz.uco.tsadv.beans;
+
+import com.haulmont.cuba.core.entity.Entity;
+import com.haulmont.cuba.core.entity.SoftDelete;
+import com.haulmont.cuba.core.entity.Updatable;
+import kz.uco.base.common.BaseCommonUtils;
+import kz.uco.base.entity.abstraction.AbstractTimeBasedEntity;
+import org.eclipse.persistence.descriptors.DescriptorEvent;
+
+public class EclipseLinkDescriptorEventListener extends com.haulmont.cuba.core.sys.persistence.EclipseLinkDescriptorEventListener {
+    @Override
+    public void prePersist(DescriptorEvent event) {
+        Entity entity = (Entity) event.getObject();
+        if ((entity instanceof AbstractTimeBasedEntity)
+                && ((AbstractTimeBasedEntity) entity).getEndDate().before(BaseCommonUtils.getSystemDate())) {
+            if (!justDeleted((SoftDelete) entity)) {
+                Updatable updatable = (Updatable) event.getObject();
+                updatable.setUpdatedBy(auditInfoProvider.getCurrentUserLogin());
+                updatable.setUpdateTs(timeSource.currentTimestamp());
+            }
+            return;
+        }
+        super.prePersist(event);
+    }
+}
