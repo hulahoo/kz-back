aebc68ab8cce5f5189aa80ac2f0b598202f5946a
Zhan <zhan.aubakirov@uco.kz>
2020-11-05 22:29:21 +0600

    [TS-923] Delete base theme based on halo.

--- modules/web/themes/base/qrcode/config.js
+++
@@ -1,9 +0,0 @@
-var config = {
-    // appUrl: "http://apps.uco.kz:8089/aa",
-    // appUrl: "http://kchrdev.air-astana.net/aa",
-    appUrl: window.location.origin + "/aa",
-    fileUrl: "/rest/v2/files",
-    clientSecret: "client:secret",//[rest.api.client]:[rest.api.secret]
-    defaultUser: "admin",
-    defaultPassword: "admin"
-};
