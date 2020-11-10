838f8ddadabb8c474025afa7baac923805d3924e
Aldiyar <aldiyar.sakhan@uco.kz>
2020-11-10 15:44:16 +0600

    TS-930 fix. changed field dicCompany

--- modules/core/db/update/postgres/20/201109-2-updateBaseOrganizationGroup02.sql
+++ modules/core/db/update/postgres/20/201109-2-updateBaseOrganizationGroup02.sql
@@ -1,4 +1,4 @@
 -- alter table BASE_ORGANIZATION_GROUP add column COMPANY_ID uuid ^
 -- update BASE_ORGANIZATION_GROUP set COMPANY_ID = <default_value> ;
 -- alter table BASE_ORGANIZATION_GROUP alter column COMPANY_ID set not null ;
-alter table BASE_ORGANIZATION_GROUP add column COMPANY_ID uuid not null ;
+alter table BASE_ORGANIZATION_GROUP add column COMPANY_ID uuid ;
