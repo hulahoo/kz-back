9afa0f8f12683e1448ba04ff496e076b20cdb1da
yerassyl.maikhanov <yerassyl.maikhanov@uco.kz>
2020-12-23 12:36:14 +0600

    KAZMIN-23 created entity

--- modules/core/db/init/postgres/10.create-db.sql
+++ modules/core/db/init/postgres/10.create-db.sql
@@ -15098,3 +15098,22 @@ create table TSADV_DIC_JOB_GROUP (
     primary key (ID)
 )^
 -- end TSADV_DIC_JOB_GROUP
+-- begin TSADV_VACATION_SCHEDULE
+create table TSADV_VACATION_SCHEDULE (
+    ID uuid,
+    VERSION integer not null,
+    CREATE_TS timestamp,
+    CREATED_BY varchar(50),
+    UPDATE_TS timestamp,
+    UPDATED_BY varchar(50),
+    DELETE_TS timestamp,
+    DELETED_BY varchar(50),
+    --
+    PERSON_GROUP_ID uuid,
+    START_DATE date,
+    END_DATE date,
+    ABSENCE_DAYS integer,
+    --
+    primary key (ID)
+)^
+-- end TSADV_VACATION_SCHEDULE
---
+++ modules/global/src/kz/uco/tsadv/entity/VacationSchedule.java
@@ -0,0 +1,65 @@
+package kz.uco.tsadv.entity;
+
+import com.haulmont.chile.core.annotations.NamePattern;
+import com.haulmont.cuba.core.entity.StandardEntity;
+import com.haulmont.cuba.core.entity.annotation.Lookup;
+import com.haulmont.cuba.core.entity.annotation.LookupType;
+import kz.uco.tsadv.modules.personal.group.PersonGroupExt;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Table(name = "TSADV_VACATION_SCHEDULE")
+@Entity(name = "tsadv_VacationSchedule")
+@NamePattern("%s(%s)|personGroup,absenceDays")
+public class VacationSchedule extends StandardEntity {
+    private static final long serialVersionUID = -8297168813225443665L;
+
+    @Lookup(type = LookupType.SCREEN, actions = "lookup")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "PERSON_GROUP_ID")
+    private PersonGroupExt personGroup;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "START_DATE")
+    private Date startDate;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "END_DATE")
+    private Date endDate;
+
+    @Column(name = "ABSENCE_DAYS")
+    private Integer absenceDays;
+
+    public void setAbsenceDays(Integer absenceDays) {
+        this.absenceDays = absenceDays;
+    }
+
+    public Integer getAbsenceDays() {
+        return absenceDays;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public PersonGroupExt getPersonGroup() {
+        return personGroup;
+    }
+
+    public void setPersonGroup(PersonGroupExt personGroup) {
+        this.personGroup = personGroup;
+    }
+}
