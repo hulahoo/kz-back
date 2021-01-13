1bce2caa797b14cce8de6601e4b007158524e3f2
Aldiyar <aldiyar.sakhan@uco.kz>
2021-01-13 16:41:55 +0600

    KAZMIN-27 fix bug in script

--- modules/core/db/init/postgres/10.create-db.sql
+++ modules/core/db/init/postgres/10.create-db.sql
@@ -16354,4 +16354,30 @@ create table TSADV_DIC_CRIMINAL_LIABILITY_TYPE (
     --
     primary key (ID)
 )^
--- end TSADV_DIC_CRIMINAL_LIABILITY_TYPE
+-- end TSADV_DIC_CRIMINAL_LIABILITY_TYPE-- begin TSADV_LEAVING_VACATION_REQUEST
+create table TSADV_LEAVING_VACATION_REQUEST (
+    ID uuid,
+    VERSION integer not null,
+    CREATE_TS timestamp,
+    CREATED_BY varchar(50),
+    UPDATE_TS timestamp,
+    UPDATED_BY varchar(50),
+    DELETE_TS timestamp,
+    DELETED_BY varchar(50),
+    LEGACY_ID varchar(255),
+    ORGANIZATION_BIN varchar(255),
+    INTEGRATION_USER_LOGIN varchar(255),
+    --
+    REQUEST_NUMBER bigint not null,
+    STATUS_REQUEST_ID uuid,
+    REQUEST_DATE date,
+    REQUEST_TYPE_ID uuid,
+    VACATION_ID uuid,
+    START_DATE date,
+    END_DATA date,
+    PLANNED_START_DATE date,
+    COMMENT_ varchar(2500),
+    --
+    primary key (ID)
+)^
+-- end TSADV_LEAVING_VACATION_REQUEST
