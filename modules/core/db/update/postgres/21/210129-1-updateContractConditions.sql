13f40cfc8e85ee42502aca2a9a5139d79c128436
Aldiyar Sakhan <aldiyar.sakhan@uco.kz>
2021-01-29 12:22:11 +0600

    KAZMIN-40 fix script

--- modules/core/db/update/postgres/21/210129-1-updateContractConditions.sql
+++
@@ -1,7 +0,0 @@
-alter table TSADV_CONTRACT_CONDITIONS rename column AGE_MAX to age_max_id__u03705;
-alter table TSADV_CONTRACT_CONDITIONS drop column if exists age_max_id__u03705;
-alter table TSADV_CONTRACT_CONDITIONS add column if not exists AGE_MAX integer not null;
-
-alter table TSADV_CONTRACT_CONDITIONS rename column COST_IN_KZT to cost_in_kzt_id__u03706;
-alter table TSADV_CONTRACT_CONDITIONS drop  column if exists age_max_id__u03706;
-alter table TSADV_CONTRACT_CONDITIONS add column if not exists COST_IN_KZT decimal(19, 2) not null;
