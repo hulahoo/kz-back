fcc4d1afcefbf314a31798c7012808a10ad626a7
Osman <niyazovo@inbox.ru>
2021-02-08 15:29:40 +0600

    Generated scripts and updated db Added DicChangeType editor screen

--- modules/core/db/init/postgres/10.create-db.sql
+++ modules/core/db/init/postgres/10.create-db.sql
@@ -16487,7 +16487,6 @@ create table TSADV_ADDRESS_FILE_DESCRIPTOR_LINK (
 )^
 -- end TSADV_ADDRESS_FILE_DESCRIPTOR_LINK
 
-
 -- begin TSADV_AWARDS_REQUEST
 create table TSADV_AWARDS_REQUEST (
     ID uuid,
@@ -17316,7 +17315,7 @@ create table TSADV_ORG_STRUCTURE_REQUEST_DETAIL (
     DELETED_BY varchar(50),
     --
     ORG_STRUCTURE_REQUEST_ID uuid,
-    CHANGE_TYPE varchar(50) not null,
+    CHANGE_TYPE_ID uuid not null,
     CURRENT_ORGANIZATION_GROUP_ID uuid,
     CURRENT_POSITION_GROUP_ID uuid,
     CURRENT_GRADE_GROUP_ID uuid,
@@ -17372,3 +17371,38 @@ create table TSADV_ORG_STRUCTURE_REQUEST_FILE_DESCRIPTOR_LINK (
     primary key (ORG_STRUCTURE_REQUEST_ID, FILE_DESCRIPTOR_ID)
 )^
 -- end TSADV_ORG_STRUCTURE_REQUEST_FILE_DESCRIPTOR_LINK
+-- begin TSADV_DIC_CHANGE_TYPE
+create table TSADV_DIC_CHANGE_TYPE (
+    ID uuid,
+    VERSION integer not null,
+    CREATE_TS timestamp,
+    CREATED_BY varchar(50),
+    UPDATE_TS timestamp,
+    UPDATED_BY varchar(50),
+    DELETE_TS timestamp,
+    DELETED_BY varchar(50),
+    LEGACY_ID varchar(255),
+    ORGANIZATION_BIN varchar(255),
+    INTEGRATION_USER_LOGIN varchar(255),
+    COMPANY_ID uuid not null,
+    LANG_VALUE1 varchar(255) not null,
+    DESCRIPTION1 varchar(2000),
+    LANG_VALUE2 varchar(255),
+    DESCRIPTION2 varchar(2000),
+    LANG_VALUE3 varchar(255),
+    DESCRIPTION3 varchar(2000),
+    LANG_VALUE4 varchar(255),
+    DESCRIPTION4 varchar(2000),
+    LANG_VALUE5 varchar(255),
+    DESCRIPTION5 varchar(2000),
+    START_DATE date,
+    END_DATE date,
+    CODE varchar(255),
+    IS_SYSTEM_RECORD boolean not null,
+    ACTIVE boolean not null,
+    IS_DEFAULT boolean not null,
+    ORDER_ integer,
+    --
+    primary key (ID)
+)^
+-- end TSADV_DIC_CHANGE_TYPE
---
+++ modules/core/db/update/postgres/21/210208-2-updateOrgStructureRequestDetail_DropScript.sql
@@ -0,0 +1,1 @@
+alter table TSADV_ORG_STRUCTURE_REQUEST_DETAIL drop column CHANGE_TYPE__U25395 cascade ;
