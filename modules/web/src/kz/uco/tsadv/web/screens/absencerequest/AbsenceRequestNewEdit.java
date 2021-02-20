ce224e1db09186a4f9a5937022c778e1e0590700
Alibek Berdaulet <alibek.berdaulet@uco.kz>
2021-02-20 22:01:40 +0600

    new rest method tsadv_AbsenceService

--- modules/web/src/kz/uco/tsadv/web/screens/absencerequest/AbsenceRequestNewEdit.java
+++ modules/web/src/kz/uco/tsadv/web/screens/absencerequest/AbsenceRequestNewEdit.java
@@ -2,30 +2,21 @@ package kz.uco.tsadv.web.screens.absencerequest;
 
 import com.haulmont.addon.bproc.web.processform.Outcome;
 import com.haulmont.addon.bproc.web.processform.ProcessForm;
-import com.haulmont.cuba.core.entity.Entity;
 import com.haulmont.cuba.core.global.DataManager;
-import com.haulmont.cuba.core.global.View;
 import com.haulmont.cuba.gui.components.*;
 import com.haulmont.cuba.gui.model.InstanceContainer;
 import com.haulmont.cuba.gui.screen.*;
+import kz.uco.base.common.StaticVariable;
 import kz.uco.tsadv.entity.bproc.AbstractBprocRequest;
-import kz.uco.tsadv.global.common.CommonUtils;
 import kz.uco.tsadv.modules.personal.dictionary.DicAbsenceType;
-import kz.uco.tsadv.modules.personal.enums.VacationDurationType;
 import kz.uco.tsadv.modules.personal.model.AbsenceRequest;
-import kz.uco.tsadv.modules.personal.model.VacationConditions;
 import kz.uco.tsadv.service.AbsenceService;
 import kz.uco.tsadv.service.AssignmentService;
 import kz.uco.tsadv.service.EmployeeNumberService;
 import kz.uco.tsadv.web.abstraction.bproc.AbstractBprocEditor;
 
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
 import javax.inject.Inject;
 import java.util.Date;
-import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
 
 @UiController("tsadv$AbsenceRequestNew.edit")
 @UiDescriptor("absence-request-new-edit.xml")
@@ -73,7 +64,14 @@ public class AbsenceRequestNewEdit extends AbstractBprocEditor<AbsenceRequest> {
                     && "RJ".equals(e.getValue().getCode());
             distanceWorkingConfirmBox.setVisible(visible);
 
-            vacationDurationTypeField.setValue(getVacationDurationType().getId());
+            if (e.getValue() != null)
+                vacationDurationTypeField.setValue(
+                        absenceService.getVacationDurationType(
+                                userSession.getAttribute(StaticVariable.USER_PERSON_GROUP_ID),
+                                e.getValue().getId(),
+                                getEditedEntity().getDateFrom())
+                                .getId()
+                );
         });
     }
 
@@ -107,47 +105,14 @@ public class AbsenceRequestNewEdit extends AbstractBprocEditor<AbsenceRequest> {
                 if (((dateFrom = getEditedEntity().getDateFrom()) != null) &&
                         ((dateTo = getEditedEntity().getDateTo()) != null) &&
                         ((absenceType = getEditedEntity().getType()) != null)) {
-                    Integer absenceDays = VacationDurationType.WORK.equals(getVacationDurationType())
-                            ? absenceService.countBusinessDays(dateFrom, dateTo, absenceType, getEditedEntity().getAssignmentGroup())
-                            : absenceService.countAbsenceDays(dateFrom, dateTo, absenceType, getEditedEntity().getAssignmentGroup());
-                    getEditedEntity().setAbsenceDays(absenceDays);
+                    getEditedEntity().setAbsenceDays(
+                            absenceService.countDays(
+                                    dateFrom,
+                                    dateTo,
+                                    absenceType.getId(),
+                                    userSession.getAttribute(StaticVariable.USER_PERSON_GROUP_ID)));
                 }
             }
         });
-    }
-
-    @Nonnull
-    protected VacationDurationType getVacationDurationType() {
-
-        VacationDurationType vacationDurationType = getVacationDurationType(absenceRequestDc.getItem().getType());
-        if (vacationDurationType != null) return vacationDurationType;
-
-        List<VacationConditions> vacationConditionsList = dataManager.load(VacationConditions.class)
-                .query("select v from base$AssignmentExt a" +
-                        "   join tsadv$VacationConditions v " +
-                        "       on v.positionGroup = a.positionGroup " +
-                        " where a.group.id = :assignmentGroupId" +
-                        "   and :sysDate between a.startDate and a.endDate " +
-                        "   and a.primaryFlag = 'TRUE' " +
-                        "   and :sysDate between v.startDate and v.endDate " +
-                        " order by v.startDate desc ")
-                .parameter("assignmentGroupId",
-                        absenceRequestDc.getItem().getAssignmentGroup().getId())
-                .parameter("sysDate",
-                        Optional.ofNullable(absenceRequestDc.getItem().getDateFrom())
-                                .orElse(CommonUtils.getSystemDate()))
-                .view(View.LOCAL)
-                .list();
-
-        return vacationConditionsList.stream()
-                .map(VacationConditions::getVacationDurationType)
-                .filter(Objects::nonNull)
-                .findAny()
-                .orElse(VacationDurationType.CALENDAR);
-    }
-
-    @Nullable
-    protected VacationDurationType getVacationDurationType(@Nullable DicAbsenceType absenceType) {
-        return absenceType != null ? absenceType.getVacationDurationType() : null;
     }
 }
