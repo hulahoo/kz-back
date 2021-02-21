eae5371e6ed55ee019dc20acd21e909b144558a3
Osman <niyazovo@inbox.ru>
2021-02-22 03:31:31 +0600

    added PersonDismissal json classes added createOrUpdate and delete PersonDismissal methods to the rest

---
+++ modules/global/src/kz/uco/tsadv/modules/integration/jsonobject/PersonDismissalJson.java
@@ -0,0 +1,63 @@
+package kz.uco.tsadv.modules.integration.jsonobject;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.io.Serializable;
+
+public class PersonDismissalJson implements Serializable {
+    private String personId;
+    private String legacyId;
+    private String dismissalReasonCode;
+    private String dismissalDate;
+    private String companyCode;
+
+    @JsonProperty("personId")
+    public String getPersonId() {
+        return personId;
+    }
+
+    @JsonProperty("personId")
+    public void setPersonId(String personId) {
+        this.personId = personId;
+    }
+
+    @JsonProperty("legacyId")
+    public String getLegacyId() {
+        return legacyId;
+    }
+
+    @JsonProperty("legacyId")
+    public void setLegacyId(String legacyId) {
+        this.legacyId = legacyId;
+    }
+
+    @JsonProperty("dismissalReasonCode")
+    public String getDismissalReasonCode() {
+        return dismissalReasonCode;
+    }
+
+    @JsonProperty("dismissalReasonCode")
+    public void setDismissalReasonCode(String dismissalReasonCode) {
+        this.dismissalReasonCode = dismissalReasonCode;
+    }
+
+    @JsonProperty("dismissalDate")
+    public String getDismissalDate() {
+        return dismissalDate;
+    }
+
+    @JsonProperty("dismissalDate")
+    public void setDismissalDate(String dismissalDate) {
+        this.dismissalDate = dismissalDate;
+    }
+
+    @JsonProperty("companyCode")
+    public String getCompanyCode() {
+        return companyCode;
+    }
+
+    @JsonProperty("companyCode")
+    public void setCompanyCode(String companyCode) {
+        this.companyCode = companyCode;
+    }
+}
