080f6e83b02969bbd128594046c6e493932ad5eb
Dulat <dulat.issenzhulov@uco.kz>
2021-02-21 16:14:51 +0600

    KAZMIN-3 pause

--- modules/web/src/kz/uco/tsadv/web/screens/absencerequest/AbsenceRequestNewEdit.java
+++ modules/web/src/kz/uco/tsadv/web/screens/absencerequest/AbsenceRequestNewEdit.java
@@ -2,16 +2,22 @@ package kz.uco.tsadv.web.screens.absencerequest;
 
 import com.haulmont.addon.bproc.web.processform.Outcome;
 import com.haulmont.addon.bproc.web.processform.ProcessForm;
-import com.haulmont.cuba.core.entity.Entity;
+import com.haulmont.bali.util.ParamsMap;
 import com.haulmont.cuba.core.global.DataManager;
 import com.haulmont.cuba.core.global.View;
 import com.haulmont.cuba.gui.components.*;
+import com.haulmont.cuba.gui.model.CollectionContainer;
+import com.haulmont.cuba.gui.model.CollectionLoader;
 import com.haulmont.cuba.gui.model.InstanceContainer;
+import com.haulmont.cuba.gui.model.InstanceLoader;
 import com.haulmont.cuba.gui.screen.*;
+import kz.uco.tsadv.entity.VacationSchedule;
 import kz.uco.tsadv.entity.bproc.AbstractBprocRequest;
 import kz.uco.tsadv.global.common.CommonUtils;
 import kz.uco.tsadv.modules.personal.dictionary.DicAbsenceType;
 import kz.uco.tsadv.modules.personal.enums.VacationDurationType;
+import kz.uco.tsadv.modules.personal.group.PersonGroupExt;
+import kz.uco.tsadv.modules.personal.model.Absence;
 import kz.uco.tsadv.modules.personal.model.AbsenceRequest;
 import kz.uco.tsadv.modules.personal.model.VacationConditions;
 import kz.uco.tsadv.service.AbsenceService;
@@ -42,7 +48,8 @@ public class AbsenceRequestNewEdit extends AbstractBprocEditor<AbsenceRequest> {
 
     @Inject
     protected InstanceContainer<AbsenceRequest> absenceRequestDc;
-
+    @Inject
+    protected CollectionContainer<VacationSchedule> vacationSchedulesDc;
     @Inject
     protected DataManager dataManager;
     @Inject
@@ -64,6 +71,16 @@ public class AbsenceRequestNewEdit extends AbstractBprocEditor<AbsenceRequest> {
     protected Form form;
     @Inject
     protected DateField<Date> dateFromField;
+    @Inject
+    protected MessageBundle messageBundle;
+    @Inject
+    protected CollectionLoader<VacationSchedule> vacationSchedulesDl;
+    @Inject
+    protected InstanceLoader<AbsenceRequest> absenceRequestDl;
+    @Inject
+    protected LookupField<VacationSchedule> vacationScheduleField;
+//    @Inject
+//    protected LookupField<VacationSchedule> vacationScheduleField;
 
     @Subscribe
     public void onInit(InitEvent event) {
@@ -98,22 +115,57 @@ public class AbsenceRequestNewEdit extends AbstractBprocEditor<AbsenceRequest> {
         }
 
         absenceRequestDc.addItemPropertyChangeListener(e -> {
-            if ("dateTo".equals(e.getProperty()))
-                dateFromField.setRangeEnd(getEditedEntity().getDateTo());
-            if ("dateFrom".equals(e.getProperty()) || "dateTo".equals(e.getProperty()) || "type".equals(e.getProperty())) {
-                Date dateFrom;
-                Date dateTo;
-                DicAbsenceType absenceType;
-                if (((dateFrom = getEditedEntity().getDateFrom()) != null) &&
-                        ((dateTo = getEditedEntity().getDateTo()) != null) &&
-                        ((absenceType = getEditedEntity().getType()) != null)) {
-                    Integer absenceDays = VacationDurationType.WORK.equals(getVacationDurationType())
-                            ? absenceService.countBusinessDays(dateFrom, dateTo, absenceType, getEditedEntity().getAssignmentGroup())
-                            : absenceService.countAbsenceDays(dateFrom, dateTo, absenceType, getEditedEntity().getAssignmentGroup());
-                    getEditedEntity().setAbsenceDays(absenceDays);
+            itemPropertyChangeListner(e);
+
+        });
+        vacationSchedulesDl.setQuery(vacationSchedulesDl.getQuery() + " where e.personGroup.id = :personGroupId");
+        vacationSchedulesDl.setParameter("personGroupId", absenceRequestDc.getItem().getPersonGroup().getId());
+        vacationSchedulesDl.load();
+    }
+
+    protected void itemPropertyChangeListner(InstanceContainer.ItemPropertyChangeEvent<AbsenceRequest> e) {
+        if ("dateTo".equals(e.getProperty()))
+            dateFromField.setRangeEnd(getEditedEntity().getDateTo());
+        if ("dateFrom".equals(e.getProperty()) || "dateTo".equals(e.getProperty()) || "type".equals(e.getProperty())) {
+            Date dateFrom;
+            Date dateTo;
+            DicAbsenceType absenceType;
+            if (((dateFrom = getEditedEntity().getDateFrom()) != null) &&
+                    ((dateTo = getEditedEntity().getDateTo()) != null) &&
+                    ((absenceType = getEditedEntity().getType()) != null)) {
+                Integer absenceDays = VacationDurationType.WORK.equals(getVacationDurationType())
+                        ? absenceService.countBusinessDays(dateFrom, dateTo, absenceType, getEditedEntity().getAssignmentGroup())
+                        : absenceService.countAbsenceDays(dateFrom, dateTo, absenceType, getEditedEntity().getAssignmentGroup());
+                getEditedEntity().setAbsenceDays(absenceDays);
+            }
+        }
+        if ("type".equals(e.getProperty())) {
+            if (e.getItem().getType() != null && e.getItem().getPersonGroup() != null) {
+                if ("ANNUAL".equals(e.getItem().getType().getCode())) {
+                    PersonGroupExt personGroupExt = dataManager.load(PersonGroupExt.class).id(e.getItem().getPersonGroup().getId())
+                            .view("personGroup-for-absenceRequest").optional().orElse(null);
+                    if (personGroupExt != null && personGroupExt.getCompany() != null
+                            && personGroupExt.getCompany().getCode() != null
+                            && "VCM".equals(personGroupExt.getCompany().getCode())) {
+                        vacationScheduleField.setVisible(false);
+                        absenceRequestDc.getItem().setVacationSchedule(null);
+                    } else {
+                        vacationScheduleField.setVisible(true);
+                        VacationSchedule firstVacationSchedule = vacationSchedulesDc.getItems().stream().filter(
+                                vacationSchedule -> vacationSchedule.getStartDate().after(CommonUtils.getSystemDate()))
+                                .min((o1, o2) -> o1.getStartDate().before(o2.getStartDate()) ? 1 : -1).orElse(null);
+                        vacationScheduleField.setValue(firstVacationSchedule);
+                        if (firstVacationSchedule != null) {
+                            absenceRequestDc.getItem().setDateFrom(firstVacationSchedule.getStartDate());
+                            absenceRequestDc.getItem().setDateTo(firstVacationSchedule.getEndDate());
+                        }
+                    }
+                } else {
+                    vacationScheduleField.setVisible(false);
+                    absenceRequestDc.getItem().setVacationSchedule(null);
                 }
             }
-        });
+        }
     }
 
     @Nonnull
@@ -149,5 +201,22 @@ public class AbsenceRequestNewEdit extends AbstractBprocEditor<AbsenceRequest> {
     @Nullable
     protected VacationDurationType getVacationDurationType(@Nullable DicAbsenceType absenceType) {
         return absenceType != null ? absenceType.getVacationDurationType() : null;
+    }
+
+    @Subscribe
+    protected void onValidation(ValidationEvent event) {
+        if (absenceRequestDc.getItem().getDateFrom() != null && absenceRequestDc.getItem().getDateTo() != null
+                && absenceRequestDc.getItem().getPersonGroup() != null) {
+            if (dataManager.load(Absence.class).query("select e from tsadv$Absence e " +
+                    " where e.personGroup.id = :personGroupId and " +
+                    " ( e.dateFrom between :startDate and :endDate or e.dateTo between :startDate and :endDate or " +
+                    " (e.dateFrom > :startDate and e.dateTo < :endDate ) or (e.dateFrom < :startDate and e.dateTo > :endDate ) )")
+                    .setParameters(ParamsMap.of("personGroupId",
+                            absenceRequestDc.getItem().getPersonGroup().getId(),
+                            "startDate", absenceRequestDc.getItem().getDateFrom(),
+                            "endDate", absenceRequestDc.getItem().getDateTo())).list().size() > 0) {
+                event.getErrors().add(messageBundle.getMessage("absenceAlreadyExists"));
+            }
+        }
     }
 }
