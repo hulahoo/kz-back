b403ae6f09327b77453197244c25b40c210517eb
kairat <kairat.karipkul@post.com>
2021-02-25 20:39:58 +0600

    KAZMIN-143

--- modules/core/src/kz/uco/tsadv/listeners/EnrollmentChangedListener.java
+++ modules/core/src/kz/uco/tsadv/listeners/EnrollmentChangedListener.java
@@ -1,14 +1,20 @@
 package kz.uco.tsadv.listeners;
 
+import com.haulmont.cuba.core.app.FileStorageAPI;
 import com.haulmont.cuba.core.app.events.AttributeChanges;
 import com.haulmont.cuba.core.app.events.EntityChangedEvent;
+import com.haulmont.cuba.core.entity.FileDescriptor;
 import com.haulmont.cuba.core.entity.contracts.Id;
 import com.haulmont.cuba.core.global.DataManager;
+import com.haulmont.cuba.core.global.EmailAttachment;
+import com.haulmont.cuba.core.global.FileStorageException;
 import kz.uco.base.notification.NotificationSenderAPI;
 import kz.uco.tsadv.modules.administration.TsadvUser;
 import kz.uco.tsadv.modules.learning.enums.EnrollmentStatus;
 import kz.uco.tsadv.modules.learning.model.Enrollment;
 import kz.uco.tsadv.modules.performance.model.CourseTrainer;
+import kz.uco.tsadv.service.LmsService;
+import org.apache.commons.lang3.ArrayUtils;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
@@ -25,6 +31,10 @@ public class EnrollmentChangedListener {
     protected NotificationSenderAPI notificationSender;
     @Inject
     protected DataManager dataManager;
+    @Inject
+    protected LmsService lmsService;
+    @Inject
+    protected FileStorageAPI fileStorageAPI;
 
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void afterCommit(EntityChangedEvent<Enrollment, UUID> event) {
@@ -42,6 +52,10 @@ public class EnrollmentChangedListener {
                         && EnrollmentStatus.APPROVED.equals(enrollment.getStatus())) {
 
                     sendNotification(enrollment, "tdc.student.enrollmentApproved");
+                } else if (!EnrollmentStatus.COMPLETED.equals(EnrollmentStatus.fromId(oldValue))
+                        && EnrollmentStatus.COMPLETED.equals(enrollment.getStatus())) {
+
+                    sendNotificationCompleted(enrollment);
                 }
             }
         } else if (event.getType().equals(EntityChangedEvent.Type.CREATED)) {
@@ -83,4 +97,40 @@ public class EnrollmentChangedListener {
         }
     }
 
+    private void sendNotificationCompleted(Enrollment enrollment) {
+        TsadvUser user = dataManager.load(TsadvUser.class)
+                .query("select e from tsadv$UserExt e " +
+                        " where e.personGroup = :personGroup")
+                .parameter("personGroup", enrollment.getPersonGroup())
+                .list().stream().findFirst().orElse(null);
+        if (user != null) {
+
+            UUID fileDescriptorId = UUID.fromString(lmsService.getCertificate(String.valueOf(enrollment.getId())));
+
+            FileDescriptor fileDescriptor = dataManager.load(FileDescriptor.class)
+                    .query("select e from SYS_FILE e " +
+                            " where e.id = :fileDescriptorId")
+                    .parameter("fileDescriptorId", fileDescriptorId)
+                    .list().stream().findFirst().orElse(null);
+
+            if (fileDescriptor != null) {
+                EmailAttachment[] emailAttachments = new EmailAttachment[0];
+                emailAttachments = getEmailAttachments(fileDescriptor, emailAttachments);
+                Map<String, Object> map = new HashMap<>();
+                map.put("courseName", enrollment.getCourse().getName());
+                notificationSender.sendParametrizedNotification("tdc.student.enrollmentClosed",
+                        user, map, emailAttachments);
+            }
+        }
+    }
+
+    private EmailAttachment[] getEmailAttachments(FileDescriptor fileDescriptor, EmailAttachment[] emailAttachments) {
+        try {
+            EmailAttachment emailAttachment = new EmailAttachment(fileStorageAPI.loadFile(fileDescriptor), "Сертификат");
+            emailAttachments = (EmailAttachment[]) ArrayUtils.add(emailAttachments, emailAttachment);
+        } catch (FileStorageException e) {
+            e.printStackTrace();
+        }
+        return emailAttachments;
+    }
 }
