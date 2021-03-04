1eea94a10d6e4ab8239e66c63e0a0cd88e8b3d0f
Osman <osmanzhan.niyazov@uco.kz>
2021-03-04 16:06:26 +0600

    added url field to LmsSlider entity

--- modules/global/src/kz/uco/tsadv/lms/entity/LmsSlider.java
+++ modules/global/src/kz/uco/tsadv/lms/entity/LmsSlider.java
@@ -1,20 +1,17 @@
 package kz.uco.tsadv.lms.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
-import com.haulmont.cuba.core.entity.FileDescriptor;
-import javax.persistence.FetchType;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.validation.constraints.NotNull;
-import com.haulmont.cuba.core.entity.StandardEntity;
-import javax.persistence.Column;
-import kz.uco.tsadv.lms.enums.LmsSliderPosition;
 import com.haulmont.chile.core.annotations.Composition;
+import com.haulmont.cuba.core.entity.StandardEntity;
 import com.haulmont.cuba.core.entity.annotation.OnDelete;
 import com.haulmont.cuba.core.global.DeletePolicy;
-import java.util.List;
+import kz.uco.tsadv.lms.enums.LmsSliderPosition;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import java.util.List;
 
 @Table(name = "TSADV_LMS_SLIDER")
 @Entity(name = "tsadv$LmsSlider")
@@ -25,10 +22,21 @@ public class LmsSlider extends StandardEntity {
     @Column(name = "POSITION_", nullable = false)
     protected String position;
 
+    @Column(name = "URL", length = 1000)
+    protected String url;
+
     @Composition
     @OnDelete(DeletePolicy.CASCADE)
     @OneToMany(mappedBy = "slider")
     protected List<LmsSliderImage> images;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
 
     public void setImages(List<LmsSliderImage> images) {
         this.images = images;
