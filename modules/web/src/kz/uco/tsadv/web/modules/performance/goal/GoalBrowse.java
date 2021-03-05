8333c17b5caf2248d487bf4e03c5e75ee16346d1
Osman <osmanzhan.niyazov@uco.kz>
2021-03-05 17:30:06 +0600

    added goals reloading after create and edit actions

--- modules/web/src/kz/uco/tsadv/web/modules/performance/goal/GoalBrowse.java
+++ modules/web/src/kz/uco/tsadv/web/modules/performance/goal/GoalBrowse.java
@@ -2,9 +2,12 @@ package kz.uco.tsadv.web.modules.performance.goal;
 
 import com.haulmont.cuba.gui.model.CollectionLoader;
 import com.haulmont.cuba.gui.screen.*;
+import kz.uco.base.cuba.actions.CreateActionExt;
+import kz.uco.base.cuba.actions.EditActionExt;
 import kz.uco.tsadv.modules.performance.model.Goal;
 
 import javax.inject.Inject;
+import javax.inject.Named;
 
 @UiController("tsadv$Goal.browse")
 @UiDescriptor("goal-browse.xml")
@@ -13,9 +16,24 @@ import javax.inject.Inject;
 public class GoalBrowse extends StandardLookup<Goal> {
     @Inject
     protected CollectionLoader<Goal> goalsDl;
+    @Named("goalsTable.create")
+    private CreateActionExt goalsTableCreate;
+    @Named("goalsTable.edit")
+    private EditActionExt goalsTableEdit;
 
     @Subscribe
     protected void onInit(InitEvent event) {
+        reloadGoals(event);
+        goalsTableCreate.setAfterCloseHandler(afterCloseHandler -> {
+            reloadGoals(event);
+        });
+        goalsTableEdit.setAfterCloseHandler(afterCloseEvent -> {
+            reloadGoals(event);
+        });
+
+    }
+
+    private void reloadGoals(InitEvent event){
         if (event != null) {
             try {
                 MapScreenOptions mapScreenOptions = (MapScreenOptions) event.getOptions();
@@ -24,10 +42,17 @@ public class GoalBrowse extends StandardLookup<Goal> {
                             " where e.library = :library ");
                     goalsDl.setParameter("library", mapScreenOptions.getParams().get("library"));
                     goalsDl.load();
+                }else{
+                    goalsDl.setQuery("select e from tsadv$Goal e");
+                    goalsDl.load();
                 }
             } catch (Exception ignored) {
             }
+        }else{
+            goalsDl.setQuery("select e from tsadv$Goal e");
+            goalsDl.load();
         }
+
     }
 
 
