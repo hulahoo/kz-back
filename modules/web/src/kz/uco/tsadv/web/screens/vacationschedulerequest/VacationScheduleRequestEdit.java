60b409639b463209f1f2c91e31ed6b0f588a7f09
Alibek Berdaulet <alibek.berdaulet@uco.kz>
2021-03-09 04:35:04 +0600

    KAZMIN-82

--- modules/web/src/kz/uco/tsadv/web/screens/vacationschedulerequest/VacationScheduleRequestEdit.java
+++ modules/web/src/kz/uco/tsadv/web/screens/vacationschedulerequest/VacationScheduleRequestEdit.java
@@ -1,18 +1,12 @@
 package kz.uco.tsadv.web.screens.vacationschedulerequest;
 
-import com.haulmont.cuba.core.global.DataManager;
-import com.haulmont.cuba.core.global.TimeSource;
 import com.haulmont.cuba.gui.components.DateField;
-import com.haulmont.cuba.gui.components.Form;
 import com.haulmont.cuba.gui.screen.*;
 import kz.uco.tsadv.entity.VacationScheduleRequest;
 import kz.uco.tsadv.service.AbsenceBalanceService;
-import kz.uco.tsadv.service.EmployeeService;
 import kz.uco.tsadv.service.TimesheetService;
 
 import javax.inject.Inject;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
 import java.util.Date;
 
 
@@ -28,17 +22,9 @@ import java.util.Date;
 @LoadDataBeforeShow
 public class VacationScheduleRequestEdit extends StandardEditor<VacationScheduleRequest> {
     @Inject
-    protected DataManager dataManager;
-    @Inject
-    protected EmployeeService employeeService;
-    @Inject
     protected TimesheetService timesheetService;
     @Inject
-    protected TimeSource timeSource;
-    @Inject
     protected AbsenceBalanceService absenceBalanceService;
-    @Inject
-    protected Form form;
     @Inject
     protected DateField<Date> startDateField;
     @Inject
@@ -47,20 +33,10 @@ public class VacationScheduleRequestEdit extends StandardEditor<VacationSchedule
     @Subscribe
     protected void onAfterShow(AfterShowEvent event) {
         VacationScheduleRequest item = getEditedEntity();
-        if ("DRAFT".equals(item.getStatus().getCode())) {
-            startDateField.setEditable(true);
-            endDateField.setEditable(true);
-            item.setBalance(absenceBalanceService.getCurrentAbsenceDays(item.getPersonGroup()));
+        item.setBalance(absenceBalanceService.getCurrentAbsenceDays(item.getPersonGroup()));
 
-            LocalDateTime ldt = LocalDateTime.ofInstant(timeSource.currentTimestamp().toInstant(), ZoneId.systemDefault());
-            Date out = Date.from(ldt.minusDays(1).atZone(ZoneId.systemDefault()).toInstant());
-
-            startDateField.setRangeStart(out);
-            startDateField.setAutofill(true);
-
-            startDateField.addValueChangeListener(dateValueChangeEvent -> calculateDays());
-            endDateField.addValueChangeListener(dateValueChangeEvent -> calculateDays());
-        } else form.setEditable(false);
+        startDateField.addValueChangeListener(dateValueChangeEvent -> calculateDays());
+        endDateField.addValueChangeListener(dateValueChangeEvent -> calculateDays());
     }
 
     protected void calculateDays() {
