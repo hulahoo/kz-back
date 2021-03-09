f7652cc104d75efbd0795009df8300d4e6c921f6
kairat <kairat.karipkul@post.com>
2021-03-09 15:25:45 +0600

    KAZMIN-171

--- modules/web/src/kz/uco/tsadv/web/screens/homework/HomeworkEdit.java
+++ modules/web/src/kz/uco/tsadv/web/screens/homework/HomeworkEdit.java
@@ -1,54 +1,11 @@
 package kz.uco.tsadv.web.screens.homework;
 
-import com.haulmont.cuba.gui.Notifications;
-import com.haulmont.cuba.gui.ScreenBuilders;
-import com.haulmont.cuba.gui.components.Action;
-import com.haulmont.cuba.gui.components.Table;
-import com.haulmont.cuba.gui.model.CollectionLoader;
-import com.haulmont.cuba.gui.model.InstanceContainer;
-import com.haulmont.cuba.gui.model.InstanceLoader;
 import com.haulmont.cuba.gui.screen.*;
 import kz.uco.tsadv.modules.learning.model.Homework;
-import kz.uco.tsadv.modules.learning.model.StudentHomework;
-
-import javax.inject.Inject;
 
 @UiController("tsadv_Homework.edit")
 @UiDescriptor("homework-edit.xml")
 @EditedEntityContainer("homeworkDc")
+@LoadDataBeforeShow
 public class HomeworkEdit extends StandardEditor<Homework> {
-    @Inject
-    protected InstanceLoader<Homework> homeworkDl;
-    @Inject
-    protected CollectionLoader<StudentHomework> studentHomeworkDl;
-    @Inject
-    protected InstanceContainer<Homework> homeworkDc;
-    @Inject
-    protected ScreenBuilders screenBuilders;
-    @Inject
-    protected Table<StudentHomework> studentTable;
-    @Inject
-    protected Notifications notifications;
-    @Inject
-    protected MessageBundle messageBundle;
-
-    @Subscribe
-    protected void onBeforeShow(BeforeShowEvent event) {
-        homeworkDl.load();
-        studentHomeworkDl.setParameter("homework", homeworkDc.getItem());
-        studentHomeworkDl.load();
-    }
-
-    @Subscribe("studentTable.create")
-    protected void onStudentTableCreate(Action.ActionPerformedEvent event) {
-        if (homeworkDc.getItem().getInstructions() != null) {
-            screenBuilders.editor(studentTable)
-                    .newEntity()
-                    .withInitializer(studentHomework -> studentHomework.setHomework(homeworkDc.getItem())).build().show()
-                    .addAfterCloseListener(afterCloseEvent -> studentHomeworkDl.load());
-        } else {
-            notifications.create().withPosition(Notifications.Position.BOTTOM_RIGHT)
-                    .withCaption(messageBundle.getMessage("fillInstructions")).show();
-        }
-    }
 }
