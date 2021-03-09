208e0967d7d7a3f153ed292eeb81a316a563a0d4
Alibek Berdaulet <alibek.berdaulet@uco.kz>
2021-03-09 21:53:59 +0600

    KAZMIN-188 create CoursePersonNote

--- modules/core/db/init/postgres/10.create-db.sql
+++ modules/core/db/init/postgres/10.create-db.sql
@@ -17721,3 +17721,21 @@ create table TSADV_DIC_ASSESSMENT_TYPE (
     primary key (ID)
 )^
 -- end TSADV_DIC_ASSESSMENT_TYPE
+-- begin TSADV_COURSE_PERSON_NOTE
+create table TSADV_COURSE_PERSON_NOTE (
+    ID uuid,
+    VERSION integer not null,
+    CREATE_TS timestamp,
+    CREATED_BY varchar(50),
+    UPDATE_TS timestamp,
+    UPDATED_BY varchar(50),
+    DELETE_TS timestamp,
+    DELETED_BY varchar(50),
+    --
+    COURSE_ID uuid not null,
+    PERSON_GROUP_ID uuid not null,
+    NOTE varchar(2000) not null,
+    --
+    primary key (ID)
+)^
+-- end TSADV_COURSE_PERSON_NOTE
