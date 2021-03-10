9e94ede480adbabdd0a3a724f4cf07f0ec8ad78f
Alibek Berdaulet <alibek.berdaulet@uco.kz>
2021-03-10 17:49:35 +0600

    KAZMIN-189 KAZMIN-191

--- modules/core/src/kz/uco/tsadv/service/CommonReportsServiceBean.java
+++ modules/core/src/kz/uco/tsadv/service/CommonReportsServiceBean.java
@@ -1,7 +1,6 @@
 package kz.uco.tsadv.service;
 
 import com.haulmont.bali.util.ParamsMap;
-import com.haulmont.cuba.core.Persistence;
 import com.haulmont.cuba.core.app.FileStorageAPI;
 import com.haulmont.cuba.core.entity.FileDescriptor;
 import com.haulmont.cuba.core.global.DataManager;
@@ -11,9 +10,11 @@ import com.haulmont.cuba.core.global.TimeSource;
 import com.haulmont.reports.app.service.ReportService;
 import com.haulmont.reports.entity.Report;
 import com.haulmont.yarg.reporting.ReportOutputDocument;
+import kz.uco.base.entity.dictionary.DicCompany;
 import kz.uco.tsadv.modules.personal.model.CertificateRequest;
 import org.apache.commons.io.FilenameUtils;
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Nonnull;
@@ -22,7 +23,7 @@ import javax.inject.Inject;
 @Service(CommonReportsService.NAME)
 public class CommonReportsServiceBean implements CommonReportsService {
 
-    protected static final Logger log = org.slf4j.LoggerFactory.getLogger(CommonReportsServiceBean.class);
+    protected static final Logger log = LoggerFactory.getLogger(CommonReportsServiceBean.class);
     @Inject
     protected DataManager dataManager;
     @Inject
@@ -34,15 +35,25 @@ public class CommonReportsServiceBean implements CommonReportsService {
     @Inject
     protected FileStorageAPI fileStorageAPI;
     @Inject
-    protected Persistence persistence;
+    protected EmployeeService employeeService;
 
     public FileDescriptor getReportByCertificateRequest(@Nonnull CertificateRequest request) {
+
+        DicCompany company = employeeService.getCompanyByPersonGroupId(request.getPersonGroup().getId());
+
         Report report = dataManager.load(Report.class)
-                .query("select e.report from tsadv_CertificateTemplate e where e.language=:langId and e.receivingType=:recTyId and e.showSalary = :salary and e.certificateType = :certType")
+                .query("select e.report from tsadv_CertificateTemplate e " +
+                        "   join e.organization.company c " +
+                        "   where e.language=:langId " +
+                        "       and e.receivingType=:recTyId " +
+                        "       and e.showSalary = :salary " +
+                        "       and e.certificateType = :certType" +
+                        "       and c.id = :companyId ")
                 .parameter("langId", request.getLanguage())
                 .parameter("recTyId", request.getReceivingType())
                 .parameter("certType", request.getCertificateType())
                 .parameter("salary", request.getShowSalary())
+                .parameter("companyId", company.getId())
                 .view("report.edit")
                 .optional().orElse(null);
 
