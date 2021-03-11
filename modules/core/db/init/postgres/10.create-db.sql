fc84b024bfbac33a95f16f0a678e4813d820d5aa
Osman <osmanzhan.niyazov@uco.kz>
2021-03-11 14:33:13 +0600

    generated db scripts for CourseCertificate entity

--- modules/core/db/init/postgres/10.create-db.sql
+++ modules/core/db/init/postgres/10.create-db.sql
@@ -17734,3 +17734,22 @@ create table TSADV_COURSE_PERSON_NOTE (
     primary key (ID)
 )^
 -- end TSADV_COURSE_PERSON_NOTE
+-- begin TSADV_COURSE_CERTIFICATE
+create table TSADV_COURSE_CERTIFICATE (
+    ID uuid,
+    VERSION integer not null,
+    CREATE_TS timestamp,
+    CREATED_BY varchar(50),
+    UPDATE_TS timestamp,
+    UPDATED_BY varchar(50),
+    DELETE_TS timestamp,
+    DELETED_BY varchar(50),
+    --
+    CERTIFICATE_ID uuid not null,
+    COURSE_ID uuid not null,
+    START_DATE date not null,
+    END_DATE date not null,
+    --
+    primary key (ID)
+)^
+-- end TSADV_COURSE_CERTIFICATE
