a15277cd2fa8475afe98c7402e02b270d707c27c
Osman <osmanzhan.niyazov@uco.kz>
2021-03-18 11:15:19 +0600

    added implementation BeforeInsertListener to AbsenceRequestListener

--- modules/core/src/kz/uco/tsadv/listeners/AbsenceRequestListener.java
+++ modules/core/src/kz/uco/tsadv/listeners/AbsenceRequestListener.java
@@ -1,6 +1,7 @@
 package kz.uco.tsadv.listeners;
 
 import com.haulmont.cuba.core.EntityManager;
+import com.haulmont.cuba.core.listener.BeforeInsertEntityListener;
 import com.haulmont.cuba.core.listener.BeforeUpdateEntityListener;
 import kong.unirest.HttpResponse;
 import kong.unirest.Unirest;
@@ -13,7 +14,7 @@ import org.springframework.stereotype.Component;
 import javax.inject.Inject;
 
 @Component("tsadv_AbsenceRequestListener")
-public class AbsenceRequestListener implements BeforeUpdateEntityListener<AbsenceRequest> {
+public class AbsenceRequestListener implements BeforeUpdateEntityListener<AbsenceRequest>, BeforeInsertEntityListener<AbsenceRequest> {
 
     private final String APPROVED_STATUS = "Утверждено";
 
@@ -22,6 +23,50 @@ public class AbsenceRequestListener implements BeforeUpdateEntityListener<Absenc
 
     @Override
     public void onBeforeUpdate(AbsenceRequest entity, EntityManager entityManager) {
+        if (entity.getStatus().getLangValue1() != null && entity.getStatus().getLangValue1().equals(APPROVED_STATUS)) {
+            AbsenceRequestDataJson absenceJson = new AbsenceRequestDataJson();
+            String personId = (entity.getPersonGroup() != null && entity.getPersonGroup().getLegacyId() != null) ? entity.getPersonGroup().getLegacyId() : "";
+            absenceJson.setPersonId(personId);
+            absenceJson.setRequestNumber(entity.getRequestNumber().toString());
+            absenceJson.setAbsenceTypeId(entity.getType().getLegacyId());
+            absenceJson.setStartDate(entity.getDateFrom().toString());
+            absenceJson.setEndDate(entity.getDateTo().toString());
+            absenceJson.setAbsenceDuration(entity.getAbsenceDays());
+            String purpose = (entity.getReason() != null) ? entity.getReason() : "";
+            absenceJson.setPurpose(purpose);
+            boolean isProvideSheetOfTemporary = (entity.getOriginalSheet() != null ? entity.getOriginalSheet() : false);
+            absenceJson.setIsProvideSheetOfTemporary(isProvideSheetOfTemporary);
+            absenceJson.setIsOnRotation(false);
+            String companyCode = (entity.getPersonGroup() != null
+                    && entity.getPersonGroup().getCompany() != null
+                    && entity.getPersonGroup().getCompany().getLegacyId() != null) ? entity.getPersonGroup().getCompany().getLegacyId() : "";
+            absenceJson.setCompanyCode(companyCode);
+
+            Unirest.config().setDefaultBasicAuth("ahruco", "ahruco");
+            Unirest.config().addDefaultHeader("Content-Type", "application/json");
+            Unirest.config().addDefaultHeader("Accept", "*/*");
+            Unirest.config().addDefaultHeader("Accept-Encoding", "gzip, deflate, br");
+
+            HttpResponse<String> response = Unirest.post("http://10.2.200.101:8290/api/ahruco/absence/request").body(absenceJson).asString();
+
+            BaseResult baseResult = new BaseResult();
+            String methodName = "AbsenceRequestListener.onBeforeUpdate";
+            String responseBody = response.getBody();
+            if(responseBody.contains("\"success\":\"true\"")){
+                integrationRestService.prepareSuccess(baseResult,
+                        methodName,
+                        absenceJson);
+            }else{
+                integrationRestService.prepareError(baseResult,
+                        methodName,
+                        absenceJson,
+                        responseBody);
+            }
+        }
+    }
+
+    @Override
+    public void onBeforeInsert(AbsenceRequest entity, EntityManager entityManager) {
         if (entity.getStatus().getLangValue1() != null && entity.getStatus().getLangValue1().equals(APPROVED_STATUS)) {
             AbsenceRequestDataJson absenceJson = new AbsenceRequestDataJson();
             String personId = (entity.getPersonGroup() != null && entity.getPersonGroup().getLegacyId() != null) ? entity.getPersonGroup().getLegacyId() : "";
