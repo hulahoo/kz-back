eea7ef9606bbf582eb3f64fd0f5f4487454727e2
kairat <kairat.karipkul@uco.kz>
2021-03-30 16:25:24 +0600

    KAZMIN-299

--- modules/core/db/init/postgres/10.create-db.sql
+++ modules/core/db/init/postgres/10.create-db.sql
@@ -17820,3 +17820,24 @@ create table TSADV_DIC_LEARNING_PROOF (
     primary key (ID)
 )^
 -- end TSADV_DIC_LEARNING_PROOF
+-- begin TSADV_SCORM_QUESTION_MAPPING
+create table TSADV_SCORM_QUESTION_MAPPING (
+    ID uuid,
+    VERSION integer not null,
+    CREATE_TS timestamp,
+    CREATED_BY varchar(50),
+    UPDATE_TS timestamp,
+    UPDATED_BY varchar(50),
+    DELETE_TS timestamp,
+    DELETED_BY varchar(50),
+    LEGACY_ID varchar(255),
+    ORGANIZATION_BIN varchar(255),
+    INTEGRATION_USER_LOGIN varchar(255),
+    --
+    LEARNING_OBJECT_ID uuid not null,
+    CODE varchar(255) not null,
+    QUESTION varchar(255) not null,
+    --
+    primary key (ID)
+)^
+-- end TSADV_SCORM_QUESTION_MAPPING
