9f1faa71a7d101ed9ec97e9312c43adde7c3f49f
Osman <osmanzhan.niyazov@uco.kz>
2021-04-01 18:17:57 +0600

    Modified AbsenceRequestListener: small changes and nonnull checking

--- modules/core/src/kz/uco/tsadv/listeners/AbsenceRequestListener.java
+++ modules/core/src/kz/uco/tsadv/listeners/AbsenceRequestListener.java
@@ -7,6 +7,7 @@ import com.haulmont.cuba.core.listener.BeforeUpdateEntityListener;
 import kong.unirest.HttpResponse;
 import kong.unirest.Unirest;
 import kz.uco.tsadv.api.BaseResult;
+import kz.uco.tsadv.api.Null;
 import kz.uco.tsadv.modules.integration.jsonobject.AbsenceRequestDataJson;
 import kz.uco.tsadv.modules.personal.dictionary.DicRequestStatus;
 import kz.uco.tsadv.modules.personal.model.AbsenceRequest;
@@ -14,11 +15,14 @@ import kz.uco.tsadv.service.IntegrationRestService;
 import org.springframework.stereotype.Component;
 
 import javax.inject.Inject;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Component("tsadv_AbsenceRequestListener")
 public class AbsenceRequestListener implements BeforeUpdateEntityListener<AbsenceRequest>, BeforeInsertEntityListener<AbsenceRequest> {
 
-    private final String APPROVED_STATUS = "APPROVED";
+    protected String APPROVED_STATUS = "APPROVED";
+    protected SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
     @Inject
     protected IntegrationRestService integrationRestService;
@@ -28,15 +32,14 @@ public class AbsenceRequestListener implements BeforeUpdateEntityListener<Absenc
         if (isApproved(entity, entityManager)) {
             AbsenceRequestDataJson absenceJson = getAbsenceRequestDataJson(entity);
 
-            Unirest.config().setDefaultBasicAuth("ahruco", "ahruco");
-            Unirest.config().addDefaultHeader("Content-Type", "application/json");
-            Unirest.config().addDefaultHeader("Accept", "*/*");
-            Unirest.config().addDefaultHeader("Accept-Encoding", "gzip, deflate, br");
-
-            HttpResponse<String> response = Unirest.post("http://10.2.200.101:8290/api/ahruco/absence/request").body(absenceJson).asString();
+            setupUnirest();
+            HttpResponse<String> response = Unirest
+                    .post("http://10.2.200.101:8290/api/ahruco/absence/request")
+                    .body(absenceJson)
+                    .asString();
 
             BaseResult baseResult = new BaseResult();
-            String methodName = "AbsenceRequestListener.onBeforeUpdate";
+            String methodName = "tsadv_AbsenceRequestListener.onBeforeUpdate";
             String responseBody = response.getBody();
             if (responseBody.contains("\"success\":\"true\"")) {
                 integrationRestService.prepareSuccess(baseResult,
@@ -56,15 +59,14 @@ public class AbsenceRequestListener implements BeforeUpdateEntityListener<Absenc
         if (isApproved(entity, entityManager)) {
             AbsenceRequestDataJson absenceJson = getAbsenceRequestDataJson(entity);
 
-            Unirest.config().setDefaultBasicAuth("ahruco", "ahruco");
-            Unirest.config().addDefaultHeader("Content-Type", "application/json");
-            Unirest.config().addDefaultHeader("Accept", "*/*");
-            Unirest.config().addDefaultHeader("Accept-Encoding", "gzip, deflate, br");
-
-            HttpResponse<String> response = Unirest.post("http://10.2.200.101:8290/api/ahruco/absence/request").body(absenceJson).asString();
+            setupUnirest();
+            HttpResponse<String> response = Unirest
+                    .post("http://10.2.200.101:8290/api/ahruco/absence/request")
+                    .body(absenceJson)
+                    .asString();
 
             BaseResult baseResult = new BaseResult();
-            String methodName = "AbsenceRequestListener.onBeforeInsert";
+            String methodName = "tsadv_AbsenceRequestListener.onBeforeInsert";
             String responseBody = response.getBody();
             if (responseBody.contains("\"success\":\"true\"")) {
                 integrationRestService.prepareSuccess(baseResult,
@@ -89,14 +91,19 @@ public class AbsenceRequestListener implements BeforeUpdateEntityListener<Absenc
         AbsenceRequestDataJson absenceJson = new AbsenceRequestDataJson();
         String personId = (entity.getPersonGroup() != null && entity.getPersonGroup().getLegacyId() != null) ? entity.getPersonGroup().getLegacyId() : "";
         absenceJson.setPersonId(personId);
-        absenceJson.setRequestNumber(entity.getRequestNumber().toString());
-        absenceJson.setAbsenceTypeId(entity.getType().getLegacyId());
-        absenceJson.setStartDate(entity.getDateFrom().toString());
-        absenceJson.setEndDate(entity.getDateTo().toString());
-        absenceJson.setAbsenceDuration(entity.getAbsenceDays());
+        String requestNumber = (entity.getRequestNumber() != null) ? entity.getRequestNumber().toString() : "";
+        absenceJson.setRequestNumber(requestNumber);
+        String absenceTypeId = (entity.getType() != null && entity.getType().getLegacyId() != null) ? entity.getType().getLegacyId() : "";
+        absenceJson.setAbsenceTypeId(absenceTypeId);
+        String startDate = getFormattedDateString(entity.getDateFrom());
+        absenceJson.setStartDate(startDate);
+        String endDate = getFormattedDateString(entity.getDateTo());
+        absenceJson.setEndDate(endDate);
+        String absenceDuration = (entity.getAbsenceDays() != null) ? entity.getAbsenceDays().toString() : "";
+        absenceJson.setAbsenceDuration(absenceDuration);
         String purpose = (entity.getReason() != null) ? entity.getReason() : "";
         absenceJson.setPurpose(purpose);
-        boolean isProvideSheetOfTemporary = (entity.getOriginalSheet() != null ? entity.getOriginalSheet() : false);
+        boolean isProvideSheetOfTemporary = Null.nullReplace(entity.getOriginalSheet(),false);
         absenceJson.setIsProvideSheetOfTemporary(isProvideSheetOfTemporary);
         absenceJson.setIsOnRotation(false);
         String companyCode = (entity.getPersonGroup() != null
@@ -104,5 +111,16 @@ public class AbsenceRequestListener implements BeforeUpdateEntityListener<Absenc
                 && entity.getPersonGroup().getCompany().getLegacyId() != null) ? entity.getPersonGroup().getCompany().getLegacyId() : "";
         absenceJson.setCompanyCode(companyCode);
         return absenceJson;
+    }
+
+    protected String getFormattedDateString(Date date){
+        return date != null ? formatter.format(date) : "" ;
+    }
+
+    protected void setupUnirest(){
+        Unirest.config().setDefaultBasicAuth("ahruco", "ahruco");
+        Unirest.config().addDefaultHeader("Content-Type", "application/json");
+        Unirest.config().addDefaultHeader("Accept", "*/*");
+        Unirest.config().addDefaultHeader("Accept-Encoding", "gzip, deflate, br");
     }
 }
