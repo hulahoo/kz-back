02cf826c9b31316a260edb8469771a6b913953b0
kairat <kairat.karipkul@uco.kz>
2021-04-06 16:29:30 +0600

    KAZMIN-339

--- modules/web/src/kz/uco/tsadv/web/modules/personal/hierarchyelement/old/v68/HierarchyElementLookup.java
+++ modules/web/src/kz/uco/tsadv/web/modules/personal/hierarchyelement/old/v68/HierarchyElementLookup.java
@@ -1,221 +1,139 @@
 package kz.uco.tsadv.web.modules.personal.hierarchyelement.old.v68;
 
+import com.haulmont.cuba.core.global.Messages;
+import com.haulmont.cuba.core.global.Metadata;
 import com.haulmont.cuba.core.global.UserSessionSource;
-import com.haulmont.cuba.gui.WindowParam;
-import com.haulmont.cuba.gui.components.AbstractLookup;
+import com.haulmont.cuba.gui.Notifications;
 import com.haulmont.cuba.gui.components.LookupField;
 import com.haulmont.cuba.gui.components.TextField;
 import com.haulmont.cuba.gui.components.Tree;
-import com.haulmont.cuba.gui.data.HierarchicalDatasource;
+import com.haulmont.cuba.gui.model.CollectionContainer;
+import com.haulmont.cuba.gui.model.CollectionLoader;
+import com.haulmont.cuba.gui.screen.*;
 import kz.uco.base.entity.shared.Hierarchy;
 import kz.uco.base.service.common.CommonService;
-import kz.uco.tsadv.config.OrganizationStructureConfig;
-import kz.uco.tsadv.config.PositionStructureConfig;
-import kz.uco.tsadv.global.common.CommonUtils;
 import kz.uco.tsadv.modules.personal.model.HierarchyElementExt;
-import org.apache.commons.lang3.StringUtils;
+import kz.uco.tsadv.service.HierarchyService;
 
 import javax.inject.Inject;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.List;
 
-//todo
-public class HierarchyElementLookup extends AbstractLookup {
+@UiController("base$HierarchyElement.lookup")
+@UiDescriptor("hierarchy-element-lookup.xml")
+@LookupComponent("tree")
+public class HierarchyElementLookup extends StandardLookup<HierarchyElementExt> {
 
     @Inject
     protected CommonService commonService;
 
     @Inject
     protected UserSessionSource userSessionSource;
-
-    @Inject
-    protected PositionStructureConfig positionStructureConfig;
-
-    @Inject
-    protected OrganizationStructureConfig organizationStructureConfig;
-
     @Inject
     protected TextField<String> searchField;
-
     @Inject
-    protected LookupField hierarchyLookup;
-
+    protected LookupField<Hierarchy> hierarchyLookup;
     @Inject
-    protected HierarchicalDatasource<HierarchyElementExt, UUID> hierarchyElementsDs;
-
+    protected Tree<HierarchyElementExt> tree;
     @Inject
-    protected Tree<HierarchyElementExt> hierarchyElementsTree;
+    protected CollectionContainer<Hierarchy> hierarchiesDc;
+    @Inject
+    protected CollectionContainer<HierarchyElementExt> hierarchyElementDc;
+    @Inject
+    protected HierarchyService hierarchyService;
+    @Inject
+    protected Messages messages;
+    @Inject
+    protected Notifications notifications;
+    @Inject
+    protected Metadata metadata;
 
-    @WindowParam
-    protected Date hierarchyEndDate;
+    protected boolean isSearch;
+    @Inject
+    protected CollectionLoader<Hierarchy> hierarchiesDl;
 
-    protected Date oracleMaxDate;
+    @Subscribe
+    protected void onBeforeShow(BeforeShowEvent event) {
+        hierarchiesDl.load();
+        refresh();
+    }
 
-    @Override
-    public void init(Map<String, Object> params) {
-        super.init(params);
+    @Subscribe
+    protected void onInit(InitEvent event) {
+        tree.setIconProvider(hierarchyElement -> {
+            if (hierarchyElement.getElementType() == null) return null;
+            switch (hierarchyElement.getElementType()) {
+                case ORGANIZATION:
+                    return "font-icon:BANK";
+                case POSITION:
+                    return "font-icon:BRIEFCASE";
+                default:
+                    return "font-icon:USER";
+            }
+        });
 
-        Calendar instance = Calendar.getInstance();
-        instance.setTime(CommonUtils.getEndOfTime());
-        instance.set(Calendar.YEAR, 4712);
-        oracleMaxDate = instance.getTime();
+        searchField.addEnterPressListener(enterPressEvent -> search());
 
-        searchField.addEnterPressListener(enterPressEvent -> searchForHierarchyElement());
-
-        if (params.containsKey("hierarchy")) {
-            hierarchyLookup.setValue(params.get("hierarchy"));
+        MapScreenOptions options = (MapScreenOptions) event.getOptions();
+        if (options.getParams().containsKey("hierarchy")) {
+            hierarchyLookup.setValue((Hierarchy) options.getParams().get("hierarchy"));
             hierarchyLookup.setEnabled(false);
         }
 
-        hierarchyLookup.addValueChangeListener(e -> hierarchyElementsDs.refresh());
-
-        hierarchyElementsTree.setIconProvider(this::getIconElement);
-
-        params.putIfAbsent("hierarchyEndDate", CommonUtils.getSystemDate());
-
-        hierarchyEndDate = (Date) params.get("hierarchyEndDate");
-        if (hierarchyEndDate.after(oracleMaxDate)) {
-            hierarchyEndDate = oracleMaxDate;
-        }
-        params.put("date", hierarchyEndDate);
-
+        hierarchyLookup.addValueChangeListener(e -> refresh());
     }
 
-    protected String getIconElement(HierarchyElementExt entity) {
-        switch (entity.getElementType()) {
-            case ORGANIZATION:
-                return "font-icon:BANK"; //BUILDING_O
-            case POSITION:
-                return "font-icon:BRIEFCASE";
-            default:
-                return "font-icon:USER";
+    public void search() {
+        String searchText = searchField.getValue();
+        if (searchText == null) {
+            refresh();
+            return;
+        } else if (searchText.length() <= 3) {
+            notifications.create(Notifications.NotificationType.HUMANIZED)
+                    .withDescription(messages.getMainMessage("search.text.small"))
+                    .show();
+            return;
         }
-    }
 
-    //todo нужно переделать
-    public void searchForHierarchyElement() {
-        String searchStr = searchField.getValue();
-        if (searchStr != null && !StringUtils.isBlank(searchStr)) {
+        List<HierarchyElementExt> hierarchyElementList = hierarchyService.search(hierarchiesDc.getItem(), searchText);
 
-            String language = userSessionSource.getLocale().getLanguage();
-
-            String hierarchyElement = "";
-            String elementLangValue = "";
-
-            if (((Hierarchy) hierarchyLookup.getValue()).getId().equals(organizationStructureConfig.getOrganizationStructureId())) {
-                hierarchyElement = "organizationGroup";
-                if (language.equals("ru")) {
-                    elementLangValue = "organizationNameLang1";
-                }
-                if (language.equals("kz")) {
-                    elementLangValue = "organizationNameLang2";
-                }
-                if (language.equals("en")) {
-                    elementLangValue = "organizationNameLang3";
-                }
-            }
-
-            if (((Hierarchy) hierarchyLookup.getValue()).getId().equals(positionStructureConfig.getPositionStructureId())) {
-                hierarchyElement = "positionGroup";
-                if (language.equals("ru")) {
-                    elementLangValue = "positionFullNameLang1";
-                }
-                if (language.equals("kz")) {
-                    elementLangValue = "positionFullNameLang1";
-                }
-                if (language.equals("en")) {
-                    elementLangValue = "positionFullNameLang1";
-                }
-            }
-
-            if (!StringUtils.isBlank(hierarchyElement) && !StringUtils.isBlank(elementLangValue)) {
-                List<HierarchyElementExt> childrenElements;
-//                if (hierarchyStartDate == null && hierarchyEndDate == null) {
-                String queryForChildren = "SELECT e\n" +
-                        " FROM base$HierarchyElementExt e\n" +
-                        " JOIN e." + hierarchyElement + " AS og\n" +
-                        " JOIN og.list AS o\n" +
-                        " WHERE :systemDate BETWEEN e.startDate AND e.endDate\n" +
-                        " AND e.hierarchy.id = :hierarchyLookup\n" +
-                        " AND LOWER(o." + elementLangValue + ") LIKE LOWER(CONCAT( '%', :searchStr, '%' ))";
-                Map<String, Object> params = new HashMap<>();
-                params.put("searchStr", searchStr);
-                params.put("systemDate", hierarchyEndDate);
-                params.put("hierarchyLookup", ((Hierarchy) hierarchyLookup.getValue()).getId());
-                childrenElements = commonService.getEntities(HierarchyElementExt.class, queryForChildren, params, "hierarchyElement.parent");
-
-                if (!childrenElements.isEmpty()) {
-                    //создаем копию списка для поиска "внучек"
-                    List<UUID> elementsIds = new ArrayList<>();
-                    //достаем родителей
-                    for (HierarchyElementExt element : childrenElements) {
-                        getParents(element, elementsIds);
-                    }
-                    String query = "SELECT e\n" +
-                            " FROM base$HierarchyElementExt e\n" +
-                            " WHERE e.id IN :custom$elementsIds\n" +
-                            " AND :param$date between e.startDate and e.endDate";
-                    Map<String, Object> map = new HashMap<>();
-                    map.put("elementsIds", elementsIds);
-                    hierarchyElementsDs.setQuery(query);
-                    hierarchyElementsDs.refresh(map);
-                    hierarchyElementsTree.expandTree();
-                } else {
-                    hierarchyElementsDs.setQuery("select e\n" +
-                            "                           from base$HierarchyElementExt e\n" +
-                            "                          where :param$date between e.startDate and e.endDate\n" +
-                            "                            and e.hierarchy.id = :component$hierarchyLookup.id");
-                    hierarchyElementsDs.refresh();
-                    hierarchyElementsTree.collapseTree();
-                }
-            }
+        if (hierarchyElementList.isEmpty()) {
+            refresh();
         } else {
-            hierarchyElementsDs.setQuery("select e\n" +
-                    "                           from base$HierarchyElementExt e\n" +
-                    "                          where :param$date between e.startDate and e.endDate\n" +
-                    "                            and e.hierarchy.id = :component$hierarchyLookup.id");
-            hierarchyElementsDs.refresh();
-            hierarchyElementsTree.collapseTree();
+            isSearch = true;
+
+            List<HierarchyElementExt> hierarchyElementDcMutableItems = hierarchyElementDc.getMutableItems();
+            hierarchyElementDcMutableItems.clear();
+            hierarchyElementDcMutableItems.addAll(hierarchyElementList);
+            tree.expandTree();
+
+            isSearch = false;
         }
+    }
 
-        if(hierarchyElementsDs.getItems() != null && !hierarchyElementsDs.getItems().isEmpty()) {
-            List<HierarchyElementExt> items = new ArrayList<>();
+    protected void refresh() {
+        Hierarchy hierarchy = hierarchiesDc.getItemOrNull();
+        if (hierarchy != null) {
+            tree.collapseTree();
+            hierarchyElementDc.getMutableItems().clear();
+            addChildren(null);
+        }
+    }
 
-            if (((Hierarchy) hierarchyLookup.getValue()).getId().equals(organizationStructureConfig.getOrganizationStructureId())) {
-                hierarchyElementsDs.getItems()
-                        .stream()
-                        .filter(element -> element != null
-                            && element.getOrganizationGroup() != null
-                            && element.getOrganizationGroup().getOrganization() == null)
-                        .collect(Collectors.toList())
-                        .forEach(items::add);
-                }
-
-            if (((Hierarchy) hierarchyLookup.getValue()).getId().equals(positionStructureConfig.getPositionStructureId())) {
-                hierarchyElementsDs.getItems()
-                        .stream()
-                        .filter(element -> element != null
-                                && element.getPositionGroup() != null
-                                && element.getPositionGroup().getPosition() == null)
-                        .collect(Collectors.toList())
-                        .forEach(items::add);
-            }
-
-            for (HierarchyElementExt element : items) {
-                hierarchyElementsDs.removeItem(hierarchyElementsDs.getItem(element.getId()));
+    protected void addChildren(HierarchyElementExt parent) {
+        List<HierarchyElementExt> childHierarchyElement = hierarchyService.getChildHierarchyElement(hierarchiesDc.getItem(), parent);
+        for (HierarchyElementExt HierarchyElementExt : childHierarchyElement) {
+            hierarchyElementDc.getMutableItems().add(HierarchyElementExt);
+            if (!isSearch && HierarchyElementExt.getHasChild()) {
+                hierarchyElementDc.getMutableItems().add(createFakeChild(HierarchyElementExt));
             }
         }
     }
 
-    protected void getParents(HierarchyElementExt element, List<UUID> list) {
-        list.add(element.getId());
-        if (element.getParent() != null) {
-            String queryString = "SELECT e FROM base$HierarchyElementExt e\n" +
-                    "WHERE e.id = :id";
-            Map<String, Object> map = new HashMap<>();
-            map.put("id", element.getParent().getId());
-            HierarchyElementExt elementExt = commonService.getEntity(HierarchyElementExt.class, queryString, map, "hierarchyElement.parent");
-            getParents(elementExt, list);
-        }
+    protected HierarchyElementExt createFakeChild(HierarchyElementExt HierarchyElementExt) {
+        HierarchyElementExt fakeChild = metadata.create(HierarchyElementExt.class);
+        fakeChild.setParent(HierarchyElementExt);
+        fakeChild.setParentFromGroup(HierarchyElementExt);
+        fakeChild.setParentName("fake");
+        return fakeChild;
     }
 }
