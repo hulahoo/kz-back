3fb0d74eedd78acff94387288dab17f897006bfa
kairat <kairat.karipkul@uco.kz>
2021-04-08 10:22:45 +0600

    KAZMIN-330

--- modules/web/src/kz/uco/tsadv/web/modules/performance/assignedperformanceplan/AssignedPerformancePlanForChangeStatusEdit.java
+++ modules/web/src/kz/uco/tsadv/web/modules/performance/assignedperformanceplan/AssignedPerformancePlanForChangeStatusEdit.java
@@ -2,6 +2,7 @@ package kz.uco.tsadv.web.modules.performance.assignedperformanceplan;
 
 import com.haulmont.cuba.core.global.CommitContext;
 import com.haulmont.cuba.core.global.DataManager;
+import com.haulmont.cuba.gui.Notifications;
 import com.haulmont.cuba.gui.components.Button;
 import com.haulmont.cuba.gui.components.HBoxLayout;
 import com.haulmont.cuba.gui.components.LookupField;
@@ -31,6 +32,10 @@ public class AssignedPerformancePlanForChangeStatusEdit extends StandardEditor<A
     protected InstanceContainer<AssignedPerformancePlan> assignedPerformancePlanDc;
     @Inject
     protected HBoxLayout editActions;
+    @Inject
+    protected Notifications notifications;
+    @Inject
+    protected MessageBundle messageBundle;
 
     public void setParameter(Set<AssignedPerformancePlan> assignedPerformancePlans) {
         this.assignedPerformancePlans = assignedPerformancePlans;
@@ -38,14 +43,19 @@ public class AssignedPerformancePlanForChangeStatusEdit extends StandardEditor<A
 
     @Subscribe("ok")
     protected void onOkClick(Button.ClickEvent event) {
-        CommitContext commitContext = new CommitContext();
-        assignedPerformancePlans.forEach(assignedPerformancePlan -> {
-            assignedPerformancePlan.setStatus(statusField.getValue());
-            assignedPerformancePlan.setStage(stageField.getValue());
-            commitContext.addInstanceToCommit(assignedPerformancePlan);
-        });
-//        commitContext.addInstanceToRemove(assignedPerformancePlanDc.getItem());
-        dataManager.commit(commitContext);
-        closeWithDefaultAction();
+        if (statusField.getValue() != null && stageField.getValue() != null) {
+            CommitContext commitContext = new CommitContext();
+            assignedPerformancePlans.forEach(assignedPerformancePlan -> {
+                assignedPerformancePlan.setStatus(statusField.getValue());
+                assignedPerformancePlan.setStage(stageField.getValue());
+                commitContext.addInstanceToCommit(assignedPerformancePlan);
+            });
+            dataManager.commit(commitContext);
+            closeWithDefaultAction();
+        } else {
+            notifications.create().withCaption(messageBundle.getMessage("fillAllFields"))
+                    .withPosition(Notifications.Position.BOTTOM_RIGHT)
+                    .show();
+        }
     }
 }
