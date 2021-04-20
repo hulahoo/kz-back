1ee137c1f6ac7e5daa30ab2d873084a7b30ae814
kairat <kairat.karipkul@uco.kz>
2021-04-20 10:24:36 +0600

    KAZMIN-429

--- modules/web/src/kz/uco/tsadv/web/modules/personal/absencebalance/AbsenceBalanceBrowse.java
+++ modules/web/src/kz/uco/tsadv/web/modules/personal/absencebalance/AbsenceBalanceBrowse.java
@@ -11,6 +11,7 @@ import kz.uco.tsadv.entity.dbview.AbsenceBalanceV;
 import kz.uco.tsadv.global.common.CommonUtils;
 import kz.uco.tsadv.modules.personal.enums.VacationDurationType;
 import kz.uco.tsadv.modules.personal.group.PersonGroupExt;
+import kz.uco.tsadv.modules.personal.model.Absence;
 import kz.uco.tsadv.modules.personal.model.AbsenceBalance;
 import kz.uco.tsadv.modules.personal.model.VacationConditions;
 import kz.uco.tsadv.service.AbsenceBalanceService;
@@ -51,6 +52,7 @@ public class AbsenceBalanceBrowse extends EditableFrame {
 
     @Override
     public void init(Map<String, Object> params) {
+        absenceBalancesDs = (CollectionDatasource<AbsenceBalance, UUID>) getDsContext().get("absenceBalancesDs");
         Objects.requireNonNull(getDsContext().get("absenceBalancesVDs")).refresh();
         absenceBalancesTable.repaint();
         personGroup = (PersonGroupExt) Objects.requireNonNull(getDsContext().get("personGroupDs")).getItem();
