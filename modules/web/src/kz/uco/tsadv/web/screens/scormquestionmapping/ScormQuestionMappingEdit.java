ee831b681c4dc6cd826171254352ed73e627a6bb
kairat <kairat.karipkul@uco.kz>
2021-05-11 10:24:35 +0600

    KAZMIN-516

--- modules/web/src/kz/uco/tsadv/web/screens/scormquestionmapping/ScormQuestionMappingEdit.java
+++ modules/web/src/kz/uco/tsadv/web/screens/scormquestionmapping/ScormQuestionMappingEdit.java
@@ -1,10 +1,12 @@
 package kz.uco.tsadv.web.screens.scormquestionmapping;
 
+import com.haulmont.bali.util.ParamsMap;
 import com.haulmont.cuba.core.global.DataManager;
 import com.haulmont.cuba.core.global.PersistenceHelper;
 import com.haulmont.cuba.gui.model.InstanceContainer;
 import com.haulmont.cuba.gui.screen.*;
 import kz.uco.base.service.common.CommonService;
+import kz.uco.tsadv.modules.learning.model.LearningObject;
 import kz.uco.tsadv.modules.learning.model.ScormQuestionMapping;
 
 import javax.inject.Inject;
@@ -25,13 +27,17 @@ public class ScormQuestionMappingEdit extends StandardEditor<ScormQuestionMappin
     @Subscribe
     protected void onAfterShow(AfterShowEvent event) {
         if (PersistenceHelper.isNew(scormQuestionMappingDc.getItem())) {
-            scormQuestionMappingDc.getItem().setSerialNumber(((Integer) getMaxSerialNumber()));
+            scormQuestionMappingDc.getItem().setSerialNumber(getMaxSerialNumber(scormQuestionMappingDc.getItem().getLearningObject()) != null
+                    ? ((Integer) getMaxSerialNumber(scormQuestionMappingDc.getItem().getLearningObject())) + 1
+                    : 1);
         }
     }
 
-    protected Object getMaxSerialNumber() {
+    protected Object getMaxSerialNumber(LearningObject learningObject) {
         Object object = commonService.emNativeQuerySingleResult(Object.class,
-                "select max(e.serial_number) from TSADV_SCORM_QUESTION_MAPPING e ", null);
+                "select max(e.serial_number) from TSADV_SCORM_QUESTION_MAPPING e " +
+                        " where e.learning_object_id = ?1 ",
+                ParamsMap.of("1", learningObject.getId()));
         if (object != null) {
             return object;
         }
