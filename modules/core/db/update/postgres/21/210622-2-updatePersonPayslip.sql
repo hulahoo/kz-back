e74ffe440cec61c3bddb834ac4e47919c43ee042
kairat.karipkul@uco.kz <kairat.karipkul@uco.kz>
2021-06-23 18:58:20 +0600

    KAZMIN-631

--- modules/core/db/update/postgres/21/210622-2-updatePersonPayslip.sql
+++ modules/core/db/update/postgres/21/210622-2-updatePersonPayslip.sql
@@ -1,4 +1,19 @@
-alter table TSADV_PERSON_PAYSLIP add column PERIOD date ;
-alter table TSADV_PERSON_PAYSLIP add column LEGACY_ID varchar(255) ;
-alter table TSADV_PERSON_PAYSLIP add column INTEGRATION_USER_LOGIN varchar(255) ;
-alter table TSADV_PERSON_PAYSLIP add column ORGANIZATION_BIN varchar(255) ;
+create table TSADV_PERSON_PAYSLIP (
+    ID uuid,
+    VERSION integer not null,
+    CREATE_TS timestamp,
+    CREATED_BY varchar(50),
+    UPDATE_TS timestamp,
+    UPDATED_BY varchar(50),
+    DELETE_TS timestamp,
+    DELETED_BY varchar(50),
+    LEGACY_ID varchar(255),
+    ORGANIZATION_BIN varchar(255),
+    INTEGRATION_USER_LOGIN varchar(255),
+    --
+    PERSON_GROUP_ID uuid,
+    PERIOD date,
+    FILE_ID uuid,
+    --
+    primary key (ID)
+);
