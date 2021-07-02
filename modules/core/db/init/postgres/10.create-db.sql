0c9efffa2a3762f9fe07783700fe2b4d713e3818
Osman <osmanzhan.niyazov@uco.kz>
2021-07-02 17:04:42 +0600

    feature/KAZMIN-658: generated db scripts for OrganizationIncentiveFlag entity

--- modules/core/db/init/postgres/10.create-db.sql
+++ modules/core/db/init/postgres/10.create-db.sql
@@ -18322,3 +18322,22 @@ create table TSADV_POSITION_COURSE (
     primary key (ID)
 )^
 -- end TSADV_POSITION_COURSE
+-- begin TSADV_ORGANIZATION_INCENTIVE_FLAG
+create table TSADV_ORGANIZATION_INCENTIVE_FLAG (
+    ID uuid,
+    VERSION integer not null,
+    CREATE_TS timestamp,
+    CREATED_BY varchar(50),
+    UPDATE_TS timestamp,
+    UPDATED_BY varchar(50),
+    DELETE_TS timestamp,
+    DELETED_BY varchar(50),
+    --
+    ORGANIZATION_GROUP_ID uuid not null,
+    IS_INCENTIVE boolean not null,
+    DATE_FROM date not null,
+    DATE_TO date not null,
+    --
+    primary key (ID)
+)^
+-- end TSADV_ORGANIZATION_INCENTIVE_FLAG
