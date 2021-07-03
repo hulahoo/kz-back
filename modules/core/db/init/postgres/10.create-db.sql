52f51d71b8d238d0fea75b9cd75df13b657f9874
Osman <osmanzhan.niyazov@uco.kz>
2021-07-03 11:54:49 +0600

    feature/KAZMIN-658: generated db scripts for OrganizationIncentiveIndicators entity

--- modules/core/db/init/postgres/10.create-db.sql
+++ modules/core/db/init/postgres/10.create-db.sql
@@ -18376,3 +18376,25 @@ create table TSADV_DIC_INCENTIVE_INDICATORS (
     primary key (ID)
 )^
 -- end TSADV_DIC_INCENTIVE_INDICATORS
+-- begin TSADV_ORGANIZATION_INCENTIVE_INDICATORS
+create table TSADV_ORGANIZATION_INCENTIVE_INDICATORS (
+    ID uuid,
+    VERSION integer not null,
+    CREATE_TS timestamp,
+    CREATED_BY varchar(50),
+    UPDATE_TS timestamp,
+    UPDATED_BY varchar(50),
+    DELETE_TS timestamp,
+    DELETED_BY varchar(50),
+    --
+    ORGANIZATION_GROUP_ID uuid not null,
+    DATE_FROM date not null,
+    DATE_TO date not null,
+    INDICATOR_TYPE varchar(50) not null,
+    INDICATOR_ID uuid not null,
+    WEIGHT double precision not null,
+    RESPONSIBLE_PERSON_ID uuid not null,
+    --
+    primary key (ID)
+)^
+-- end TSADV_ORGANIZATION_INCENTIVE_INDICATORS
