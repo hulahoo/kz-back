c4c237a418601d6480d4b6b89d1129ee9a6f51b6
Osman Niyazov <osmanzhan.niyazov@uco.kz>
2021-08-03 18:44:26 +0600

    KAZMIN-743: generated db scripts for OrganizationIncentiveMonthResult entity

--- modules/core/db/init/postgres/10.create-db.sql
+++ modules/core/db/init/postgres/10.create-db.sql
@@ -18777,3 +18777,29 @@ create table TSADV_DIC_INCENTIVE_RESULT_STATUS (
     primary key (ID)
 )^
 -- end TSADV_DIC_INCENTIVE_RESULT_STATUS
+-- begin TSADV_ORGANIZATION_INCENTIVE_MONTH_RESULT
+create table TSADV_ORGANIZATION_INCENTIVE_MONTH_RESULT (
+    ID uuid,
+    VERSION integer not null,
+    CREATE_TS timestamp,
+    CREATED_BY varchar(50),
+    UPDATE_TS timestamp,
+    UPDATED_BY varchar(50),
+    DELETE_TS timestamp,
+    DELETED_BY varchar(50),
+    --
+    COMPANY_ID uuid not null,
+    PERIOD_ date not null,
+    DEPARTMENT_ID uuid not null,
+    INDICATOR_ID uuid not null,
+    WEIGHT double precision not null,
+    PLAN_ decimal(19, 2) not null,
+    FACT decimal(19, 2) not null,
+    PREMIUM_PERCENT double precision not null,
+    TOTAL_PREMIUM_PERCENT double precision not null,
+    STATUS_ID uuid not null,
+    COMMENT_ varchar(2500) not null,
+    --
+    primary key (ID)
+)^
+-- end TSADV_ORGANIZATION_INCENTIVE_MONTH_RESULT
