89956b3ca762ae43bdcaeff438604df5d42b15a7
Osman Niyazov <osmanzhan.niyazov@uco.kz>
2021-08-04 14:35:14 +0600

    KAZMIN-743: fixed 210803-2-updateDicIncentiveIndicators.sql

--- modules/core/db/update/postgres/21/210803-2-updateDicIncentiveIndicators.sql
+++ modules/core/db/update/postgres/21/210803-2-updateDicIncentiveIndicators.sql
@@ -1,6 +1,6 @@
 --INSERT DEFAULT INDICATOR_TYPE FOR ESCAPE TYPE_ID NULL ERROR 
  INSERT INTO public.tsadv_dic_incentive_indicator_type (id, "version", company_id, lang_value1, code,is_system_record,active,is_default) 
- VALUES(gen_random_uuid (), 0, (select id from base_dic_company limit 1),'DEFAULT_INDICATOR_TYPE','DEFAULT_INDICATOR_TYPE', false, false, false);
+ VALUES((md5(random()::text || clock_timestamp()::text)::uuid), 0, (select id from base_dic_company limit 1),'DEFAULT_INDICATOR_TYPE','DEFAULT_INDICATOR_TYPE', false, false, false);
  
  alter table TSADV_DIC_INCENTIVE_INDICATORS add column TYPE_ID uuid ^
  update TSADV_DIC_INCENTIVE_INDICATORS set TYPE_ID = (select id from tsadv_dic_incentive_indicator_type where code = 'DEFAULT_INDICATOR_TYPE') where TYPE_ID IS NULL ;
