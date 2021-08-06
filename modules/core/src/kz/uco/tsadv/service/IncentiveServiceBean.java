8854b707d1c3c6ed6cc926475dc09e0330ba1edc
Alibek Berdaulet <alibek.berdaulet@uco.kz>
2021-08-06 18:09:04 +0600

    KAZMIN-743 saveMonthResult

--- modules/core/src/kz/uco/tsadv/service/IncentiveServiceBean.java
+++ modules/core/src/kz/uco/tsadv/service/IncentiveServiceBean.java
@@ -2,12 +2,22 @@ package kz.uco.tsadv.service;
 
 import com.google.gson.Gson;
 import com.haulmont.cuba.core.Persistence;
+import com.haulmont.cuba.core.global.CommitContext;
+import com.haulmont.cuba.core.global.DataManager;
 import com.haulmont.cuba.core.global.UserSessionSource;
+import com.haulmont.cuba.core.global.View;
 import kz.uco.base.common.MultiLanguageUtils;
-import kz.uco.base.common.StaticVariable;
+import kz.uco.base.service.common.CommonService;
+import kz.uco.tsadv.exceptions.PortalException;
+import kz.uco.tsadv.modules.personal.dictionary.DicIncentiveResultStatus;
+import kz.uco.tsadv.modules.personal.group.PositionGroupExt;
+import kz.uco.tsadv.modules.personal.model.OrganizationIncentiveMonthResult;
+import kz.uco.tsadv.modules.personal.model.PositionExt;
 import kz.uco.tsadv.pojo.IncentivePojo;
 import kz.uco.tsadv.pojo.pagination.EntitiesPaginationResult;
 import kz.uco.tsadv.pojo.pagination.PaginationPojo;
+import kz.uco.uactivity.entity.Activity;
+import kz.uco.uactivity.entity.StatusEnum;
 import org.springframework.stereotype.Service;
 
 import javax.inject.Inject;
@@ -23,19 +33,34 @@ public class IncentiveServiceBean implements IncentiveService {
     protected UserSessionSource userSessionSource;
     @Inject
     protected Persistence persistence;
+    @Inject
+    protected PositionService positionService;
+    @Inject
+    protected DataManager dataManager;
+    @Inject
+    protected CommonService commonService;
 
     protected final static Gson gson = new Gson();
 
     @SuppressWarnings({"ConstantConditions", "unchecked"})
     @Override
     public EntitiesPaginationResult getIncentiveList(PaginationPojo paginationPojo) {
-        UUID personGroupId = userSessionSource.getUserSession().getAttribute(StaticVariable.USER_PERSON_GROUP_ID);
+        PositionExt position = positionService.getPosition(new View(PositionExt.class).addProperty("group", new View(PositionGroupExt.class)));
+
+        if (position == null || position.getGroup() == null)
+            throw new PortalException("Позиция не найдено!");
+
+        UUID positionGroupId = position.getGroup().getId();
 
         final String query = "with result as (\n" +
-                "    select sum(coalesce(r.result_, 0) * coalesce(r.weight, 0) / 100) as result,\n" +
+                "    select sum(score.total_score) as result,\n" +
                 "           r.organization_group_id,\n" +
                 "           to_char(r.period_date, 'yyyy-MM')                   as date_\n" +
                 "    from tsadv_organization_incentive_result r\n" +
+                "             left join TSADV_DIC_INCENTIVE_INDICATOR_SCORE_SETTING score\n" +
+                "                       on score.indicator_id = r.indicator_id\n" +
+                "                           and score.delete_ts is null\n" +
+                "                           and r.result_ between score.min_percent and score.max_percent" +
                 "    where r.delete_ts is null\n" +
                 "    group by r.organization_group_id, to_char(r.period_date, 'yyyy-MM')\n" +
                 "),\n" +
@@ -50,7 +75,7 @@ public class IncentiveServiceBean implements IncentiveService {
                 "                           and of.date_from <= i.date_to\n" +
                 "                  join generate_series(greatest(i.date_from, of.date_from), least(i.date_to, of.date_to), '1 month'::interval) d on 1 = 1 \n" +
                 "         where i.delete_ts is null\n" +
-                "           and i.responsible_person_id = #personGroupId\n" +
+                "           and i.RESPONSIBLE_POSITION_ID = #personGroupId\n" +
                 "         group by d, i.organization_group_id\n" +
                 "     )\n" +
                 "select %s\n" +
@@ -77,11 +102,11 @@ public class IncentiveServiceBean implements IncentiveService {
             EntitiesPaginationResult paginationResult = new EntitiesPaginationResult();
 
             paginationResult.setCount(Long.valueOf(em.createNativeQuery(String.format(query, count, ""))
-                    .setParameter("personGroupId", personGroupId)
+                    .setParameter("personGroupId", positionGroupId)
                     .getFirstResult().toString()));
 
             List<IncentivePojo> pojoList = (List<IncentivePojo>) em.createNativeQuery(String.format(query, fields, orderBy))
-                    .setParameter("personGroupId", personGroupId)
+                    .setParameter("personGroupId", positionGroupId)
                     .setFirstResult(paginationPojo.getOffset())
                     .setMaxResults(paginationPojo.getLimit())
                     .getResultList()
@@ -93,6 +118,31 @@ public class IncentiveServiceBean implements IncentiveService {
 
             return paginationResult;
         });
+    }
+
+    @Override
+    public void saveMonthResult(String status, String comment, UUID incentiveMonthResultId) {
+        OrganizationIncentiveMonthResult incentiveMonth = dataManager.load(OrganizationIncentiveMonthResult.class)
+                .id(incentiveMonthResultId)
+                .viewProperties("status", "comment")
+                .one();
+
+        incentiveMonth.setStatus(commonService.getEntity(DicIncentiveResultStatus.class, status));
+        incentiveMonth.setComment(comment);
+
+        List<Activity> activities = dataManager.load(Activity.class)
+                .query("select e from uactivity$Activity e " +
+                        "   where e.referenceId = :referenceId " +
+                        "       and e.status = :status")
+                .parameter("referenceId", incentiveMonthResultId)
+                .parameter("status", StatusEnum.active)
+                .list();
+
+        CommitContext context = new CommitContext();
+        context.addInstanceToCommit(incentiveMonth);
+        activities.stream().peek(activity -> activity.setStatus(StatusEnum.done)).forEach(context::addInstanceToCommit);
+
+        dataManager.commit(context);
     }
 
     private IncentivePojo parseToIncentivePojo(Object o) {
