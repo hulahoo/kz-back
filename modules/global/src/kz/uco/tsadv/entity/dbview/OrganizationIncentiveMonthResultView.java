7f515feef3f89b3b8753923a124954f8d9e6130a
Osman Niyazov <osmanzhan.niyazov@uco.kz>
2021-08-06 10:59:43 +0600

    KAZMIN-743: added DicIncentiveIndicatorType screens and added dbView OrganizationIncentiveMonthResultView

---
+++ modules/global/src/kz/uco/tsadv/entity/dbview/OrganizationIncentiveMonthResultView.java
@@ -0,0 +1,145 @@
+package kz.uco.tsadv.entity.dbview;
+
+import com.haulmont.cuba.core.entity.StandardEntity;
+import com.haulmont.cuba.core.global.DbView;
+import kz.uco.base.entity.dictionary.DicCompany;
+import kz.uco.tsadv.modules.personal.dictionary.DicIncentiveIndicators;
+import kz.uco.tsadv.modules.personal.dictionary.DicIncentiveResultStatus;
+import kz.uco.tsadv.modules.personal.group.OrganizationGroupExt;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@DbView
+@Table(name = "TSADV_ORGANIZATION_INCENTIVE_MONTH_RESULT_VIEW")
+@Entity(name = "tsadv_OrganizationIncentiveMonthResultView")
+public class OrganizationIncentiveMonthResultView extends StandardEntity {
+    private static final long serialVersionUID = 6512933878265314256L;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "COMPANY_ID")
+    private DicCompany company;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "PERIOD_")
+    private Date period;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "DEPARTMENT_ID")
+    private OrganizationGroupExt department;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "INDICATOR_ID")
+    private DicIncentiveIndicators indicator;
+
+    @Column(name = "WEIGHT")
+    private Double weight;
+
+    @Column(name = "PLAN_")
+    private BigDecimal plan;
+
+    @Column(name = "FACT")
+    private BigDecimal fact;
+
+    @Column(name = "PREMIUM_PERCENT")
+    private Double premiumPercent;
+
+    @Column(name = "TOTAL_PREMIUM_PERCENT")
+    private BigDecimal totalPremiumPercent;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "STATUS_ID")
+    private DicIncentiveResultStatus status;
+
+    @Column(name = "COMMENT_", length = 2500)
+    private String comment;
+
+    public void setWeight(Double weight) {
+        this.weight = weight;
+    }
+
+    public Double getWeight() {
+        return weight;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public DicIncentiveResultStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(DicIncentiveResultStatus status) {
+        this.status = status;
+    }
+
+    public BigDecimal getTotalPremiumPercent() {
+        return totalPremiumPercent;
+    }
+
+    public void setTotalPremiumPercent(BigDecimal totalPremiumPercent) {
+        this.totalPremiumPercent = totalPremiumPercent;
+    }
+
+    public Double getPremiumPercent() {
+        return premiumPercent;
+    }
+
+    public void setPremiumPercent(Double premiumPercent) {
+        this.premiumPercent = premiumPercent;
+    }
+
+    public BigDecimal getFact() {
+        return fact;
+    }
+
+    public void setFact(BigDecimal fact) {
+        this.fact = fact;
+    }
+
+    public BigDecimal getPlan() {
+        return plan;
+    }
+
+    public void setPlan(BigDecimal plan) {
+        this.plan = plan;
+    }
+
+    public DicIncentiveIndicators getIndicator() {
+        return indicator;
+    }
+
+    public void setIndicator(DicIncentiveIndicators indicator) {
+        this.indicator = indicator;
+    }
+
+    public OrganizationGroupExt getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(OrganizationGroupExt department) {
+        this.department = department;
+    }
+
+    public Date getPeriod() {
+        return period;
+    }
+
+    public void setPeriod(Date period) {
+        this.period = period;
+    }
+
+    public DicCompany getCompany() {
+        return company;
+    }
+
+    public void setCompany(DicCompany company) {
+        this.company = company;
+    }
+}
--- modules/web/src/kz/uco/tsadv/web/screens/organizationincentivemonthresult/OrganizationIncentiveMonthResultBrowse.java
+++ modules/web/src/kz/uco/tsadv/web/screens/organizationincentivemonthresult/OrganizationIncentiveMonthResultBrowse.java
@@ -1,11 +1,75 @@
 package kz.uco.tsadv.web.screens.organizationincentivemonthresult;
 
+import com.haulmont.cuba.core.global.Metadata;
+import com.haulmont.cuba.gui.components.AggregationInfo;
+import com.haulmont.cuba.gui.components.GroupTable;
+import com.haulmont.cuba.gui.components.Table;
+import com.haulmont.cuba.gui.data.aggregation.AggregationStrategy;
 import com.haulmont.cuba.gui.screen.*;
+import kz.uco.tsadv.entity.dbview.OrganizationIncentiveMonthResultView;
+import kz.uco.tsadv.modules.personal.dictionary.DicIncentiveResultStatus;
 import kz.uco.tsadv.modules.personal.model.OrganizationIncentiveMonthResult;
+
+import javax.inject.Inject;
+import java.util.Collection;
+import java.util.List;
+import java.util.Random;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @UiController("tsadv_OrganizationIncentiveMonthResult.browse")
 @UiDescriptor("organization-incentive-month-result-browse.xml")
 @LookupComponent("organizationIncentiveMonthResultsTable")
 @LoadDataBeforeShow
-public class OrganizationIncentiveMonthResultBrowse extends StandardLookup<OrganizationIncentiveMonthResult> {
+public class OrganizationIncentiveMonthResultBrowse extends StandardLookup<OrganizationIncentiveMonthResultView> {
+
+    @Inject
+    private GroupTable<OrganizationIncentiveMonthResultView> organizationIncentiveMonthResultsTable;
+    @Inject
+    private Metadata metadata;
+
+    public class EmptyStringFormatter implements Function<Object, String> {
+        @Override
+        public String apply(Object o) {
+            return "";
+        }
+
+        @Override
+        public <V> Function<V, String> compose(Function<? super V, ?> before) {
+            return null;
+        }
+
+        @Override
+        public <V> Function<Object, V> andThen(Function<? super String, ? extends V> after) {
+            return null;
+        }
+    }
+
+    protected class AggStrategy implements AggregationStrategy<DicIncentiveResultStatus,String>{
+        @Override
+        public String aggregate(Collection<DicIncentiveResultStatus> propertyValues) {
+            return propertyValues.stream().findAny().get().getLangValue();
+        }
+
+        @Override
+        public Class<String> getResultClass() {
+            return String.class;
+        }
+    }
+
+    @Subscribe
+    public void onBeforeShow(BeforeShowEvent event) {
+
+        organizationIncentiveMonthResultsTable.getColumn("status").setFormatter(new EmptyStringFormatter());
+
+        AggregationInfo info = new AggregationInfo();
+        info.setPropertyPath(metadata.getClass(OrganizationIncentiveMonthResultView.class).getPropertyPath("status"));
+        info.setStrategy(new AggStrategy());
+
+        organizationIncentiveMonthResultsTable.setAggregatable(true);
+        Table.Column column = organizationIncentiveMonthResultsTable.getColumn("status");
+        column.setAggregation(info);
+    }
+
+
 }
