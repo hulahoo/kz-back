fe7086fb2516704d91f21ce145ef11b41921d031
Alibek Berdaulet <alibek.berdaulet@uco.kz>
2021-08-10 19:35:29 +0600

    KAZMIN-687

--- modules/core/src/kz/uco/tsadv/listeners/OrganizationIncentiveMonthResultListener.java
+++ modules/core/src/kz/uco/tsadv/listeners/OrganizationIncentiveMonthResultListener.java
@@ -8,9 +8,9 @@ import kong.unirest.Unirest;
 import kz.uco.tsadv.api.BaseResult;
 import kz.uco.tsadv.api.Null;
 import kz.uco.tsadv.config.IntegrationConfig;
-import kz.uco.tsadv.entity.dbview.OrganizationIncentiveMonthResultShortView;
 import kz.uco.tsadv.modules.integration.jsonobject.OrganizationIncentiveMonthResultJson;
 import kz.uco.tsadv.modules.personal.model.OrganizationIncentiveMonthResult;
+import kz.uco.tsadv.modules.personal.model.OrganizationIncentiveResult;
 import kz.uco.tsadv.service.IntegrationRestService;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
@@ -36,62 +36,62 @@ public class OrganizationIncentiveMonthResultListener {
 
     @TransactionalEventListener(phase = TransactionPhase.BEFORE_COMMIT)
     public void beforeCommit(EntityChangedEvent<OrganizationIncentiveMonthResult, UUID> event) {
-        if(event.getType() == EntityChangedEvent.Type.CREATED || event.getType() == EntityChangedEvent.Type.UPDATED){
+        if (event.getType() == EntityChangedEvent.Type.CREATED || event.getType() == EntityChangedEvent.Type.UPDATED) {
 
             String methodName = "tsadv_OrganizationIncentiveResultListener.OnBefore" + event.getType();
             OrganizationIncentiveMonthResult incentiveMonthResult = loadEventEntity(event);
-            if(isEntityApproved(incentiveMonthResult)){
-                List<OrganizationIncentiveMonthResultShortView> incentiveMonthResultShortViews = transactionalDataManager
-                        .load(OrganizationIncentiveMonthResultShortView.class)
-                        .query("select e from tsadv_OrganizationIncentiveMonthResultShortView e where e.incentiveMonthResult = :incentiveMonthResul")
-                        .parameter("incentiveMonthResul",incentiveMonthResult)
-                        .list();
+            if (isEntityApproved(incentiveMonthResult)) {
 
+                incentiveMonthResult = dataManager.reload(incentiveMonthResult, "organizationIncentiveMonthResult.integration.full");
 
                 List<OrganizationIncentiveMonthResultJson> incentiveMonthResultJsons = new ArrayList<>();
-                for(OrganizationIncentiveMonthResultShortView incentiveMonthResultShortView : incentiveMonthResultShortViews){
-                    OrganizationIncentiveMonthResultJson incentiveMonthResultJson = getOrganizationIncentiveMonthResultJson(incentiveMonthResultShortView);
-                    incentiveMonthResultJsons.add(incentiveMonthResultJson);
-                }
 
-                sendRequest(incentiveMonthResultJsons,methodName);
+                List<OrganizationIncentiveResult> incentiveResults = incentiveMonthResult.getIncentiveResults();
+
+                incentiveMonthResult.getIncentiveResults()
+                        .stream()
+                        .map(this::parseToOrganizationIncentiveMonthResultJson)
+                        .peek(resultJson -> resultJson.setResult(
+                                String.valueOf(incentiveResults.stream()
+                                        .filter(result -> Objects.equals(result.getIndicator().getCode(), resultJson.getIncentiveType()))
+                                        .mapToDouble(OrganizationIncentiveResult::getScore)
+                                        .sum())
+                        ))
+                        .forEach(incentiveMonthResultJsons::add);
+
+                sendRequest(incentiveMonthResultJsons, methodName);
             }
 
         }
-
     }
 
-    protected OrganizationIncentiveMonthResult loadEventEntity(EntityChangedEvent<OrganizationIncentiveMonthResult, UUID> event){
+    protected OrganizationIncentiveMonthResultJson parseToOrganizationIncentiveMonthResultJson(OrganizationIncentiveResult organizationIncentiveResult) {
+        OrganizationIncentiveMonthResultJson organizationIncentiveMonthResultJson = new OrganizationIncentiveMonthResultJson();
+        String organizationId = Null.nullReplace(organizationIncentiveResult.getOrganizationGroup().getLegacyId(), "");
+        organizationIncentiveMonthResultJson.setOrganizationId(organizationId);
+        String period = formatter.format(organizationIncentiveResult.getPeriodDate());
+        organizationIncentiveMonthResultJson.setPeriod(period);
+        String incentiveType = Null.nullReplace(organizationIncentiveResult.getIndicator().getType().getCode(), "");
+        organizationIncentiveMonthResultJson.setIncentiveType(incentiveType);
+        String companyCode = Null.nullReplace(organizationIncentiveResult.getOrganizationGroup().getCompany().getLegacyId(), "");
+        organizationIncentiveMonthResultJson.setCompanyCode(companyCode);
+        return organizationIncentiveMonthResultJson;
+    }
+
+    protected OrganizationIncentiveMonthResult loadEventEntity(EntityChangedEvent<OrganizationIncentiveMonthResult, UUID> event) {
         return transactionalDataManager
                 .load(event.getEntityId())
                 .view("organizationIncentiveMonthResult.integration")
                 .one();
     }
 
-    protected boolean isEntityApproved(OrganizationIncentiveMonthResult incentiveMonthResult){
+    protected boolean isEntityApproved(OrganizationIncentiveMonthResult incentiveMonthResult) {
         return incentiveMonthResult.getStatus() != null && "APPROVED".equals(incentiveMonthResult.getStatus().getCode());
     }
 
-    protected OrganizationIncentiveMonthResultJson getOrganizationIncentiveMonthResultJson(OrganizationIncentiveMonthResultShortView incentiveMonthResult){
-        OrganizationIncentiveMonthResultJson organizationIncentiveMonthResultJson = new OrganizationIncentiveMonthResultJson();
-        incentiveMonthResult = dataManager.reload(incentiveMonthResult,"organizationIncentiveMonthResultShortView.integration");
-        String organizationId = Null.nullReplace(incentiveMonthResult.getDepartment().getLegacyId(),"");
-        organizationIncentiveMonthResultJson.setOrganizationId(organizationId);
-        String period = formatter.format(incentiveMonthResult.getPeriod());
-        organizationIncentiveMonthResultJson.setPeriod(period);
-        String incentiveType = Null.nullReplace(incentiveMonthResult.getIndicator().getType().getCode(),"");
-        organizationIncentiveMonthResultJson.setIncentiveType(incentiveType);
-        String result = String.valueOf(incentiveMonthResult.getResult());
-        organizationIncentiveMonthResultJson.setResult(result);
-        String companyCode = Null.nullReplace(incentiveMonthResult.getCompany().getLegacyId(),"");
-        organizationIncentiveMonthResultJson.setCompanyCode(companyCode);
+    protected void sendRequest(List<OrganizationIncentiveMonthResultJson> organizationIncentiveMonthResultJsons, String methodName) {
 
-        return organizationIncentiveMonthResultJson;
-    }
-
-    protected void sendRequest(List<OrganizationIncentiveMonthResultJson> organizationIncentiveMonthResultJsons,String methodName){
-
-        for(OrganizationIncentiveMonthResultJson incentiveMonthResultJson : organizationIncentiveMonthResultJsons) {
+        for (OrganizationIncentiveMonthResultJson incentiveMonthResultJson : organizationIncentiveMonthResultJsons) {
             BaseResult baseResult = new BaseResult();
             String responseBody = null;
             try {
@@ -119,23 +119,22 @@ public class OrganizationIncentiveMonthResultListener {
                         incentiveMonthResultJson,
                         logErrorMessage);
                 //todo switch to UnirestIntegrationException
-                throw new RuntimeException(exceptionMessage);
+                throw new RuntimeException(exceptionMessage, e);
             }
         }
     }
 
-    protected HttpResponse<String> doPost(OrganizationIncentiveMonthResultJson organizationIncentiveMonthResultJson){
+    protected HttpResponse<String> doPost(OrganizationIncentiveMonthResultJson organizationIncentiveMonthResultJson) {
         Map<String, String> headers = new HashMap<>();
         headers.put("Content-Type", "application/json");
         headers.put("Accept", "*/*");
         headers.put("Accept-Encoding", "gzip, deflate, br");
-        HttpResponse<String> response = Unirest
+
+        return Unirest
                 .post(integrationConfig.getOrganizationIncentiveResultIntegrationUrl())
-                .basicAuth(integrationConfig.getBasicAuthLogin(),integrationConfig.getBasicAuthPassword())
+                .basicAuth(integrationConfig.getBasicAuthLogin(), integrationConfig.getBasicAuthPassword())
                 .headers(headers)
                 .body(organizationIncentiveMonthResultJson)
                 .asString();
-
-        return response;
     }
 }
