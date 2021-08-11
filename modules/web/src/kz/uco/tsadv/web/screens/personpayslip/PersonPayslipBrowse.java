7b96e8e99bb210777b7ba49de16bf3f6aa09fee6
Osman Niyazov <osmanzhan.niyazov@uco.kz>
2021-08-11 14:17:12 +0600

    KAZMIN-631: added AccessConfig and checking access to PersonPayslipBrowse screen

--- modules/web/src/kz/uco/tsadv/web/screens/personpayslip/PersonPayslipBrowse.java
+++ modules/web/src/kz/uco/tsadv/web/screens/personpayslip/PersonPayslipBrowse.java
@@ -1,10 +1,14 @@
 package kz.uco.tsadv.web.screens.personpayslip;
 
+import com.haulmont.cuba.core.global.Messages;
+import com.haulmont.cuba.gui.Notifications;
 import com.haulmont.cuba.gui.export.ExportDisplay;
 import com.haulmont.cuba.gui.export.ExportFormat;
 import com.haulmont.cuba.gui.screen.*;
 import com.haulmont.cuba.gui.xml.layout.ComponentsFactory;
+import kz.uco.tsadv.config.AccessConfig;
 import kz.uco.tsadv.modules.personal.model.PersonPayslip;
+import kz.uco.tsadv.service.UserService;
 
 import javax.inject.Inject;
 
@@ -15,11 +19,38 @@ import javax.inject.Inject;
 public class PersonPayslipBrowse extends StandardLookup<PersonPayslip> {
 
     @Inject
+    private AccessConfig accessConfig;
+
+    @Inject
     private ExportDisplay exportDisplay;
     @Inject
     private ComponentsFactory componentsFactory;
+    @Inject
+    private UserService userService;
+    @Inject
+    private Notifications notifications;
+    @Inject
+    private Messages messages;
+    @Inject
+    private MessageBundle messageBundle;
+
+    @Subscribe
+    public void onInit(InitEvent event) {
+        checkUserAccess();
+    }
 
     public void generatorName(PersonPayslip item, String columnId) {
         exportDisplay.show(item.getFile(), ExportFormat.OCTET_STREAM);
+    }
+
+    protected void checkUserAccess(){
+        String delimiter = ";";
+        String accessRoles = accessConfig.getPersonPayslipAccessRoles();
+        String[] acessRolesArray = accessRoles.split(delimiter);
+
+        if(!userService.hasAnyRole(acessRolesArray)){
+            String noAccessMessage = messageBundle.getMessage("noAccess");
+            throw new RuntimeException(noAccessMessage);
+        }
     }
 }
