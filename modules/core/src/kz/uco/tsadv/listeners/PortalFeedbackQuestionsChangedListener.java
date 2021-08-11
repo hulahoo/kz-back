53c21c9f3b2513e297e2e60608c1eec80d0ab76e
Alibek Berdaulet <alibek.berdaulet@uco.kz>
2021-08-11 14:50:36 +0600

    KAZMIN-758

--- modules/core/src/kz/uco/tsadv/listeners/PortalFeedbackQuestionsChangedListener.java
+++ modules/core/src/kz/uco/tsadv/listeners/PortalFeedbackQuestionsChangedListener.java
@@ -5,6 +5,8 @@ import com.haulmont.cuba.core.entity.contracts.Id;
 import com.haulmont.cuba.core.global.DataManager;
 import kz.uco.base.service.NotificationService;
 import kz.uco.tsadv.modules.learning.model.PortalFeedbackQuestions;
+import kz.uco.tsadv.modules.personal.dto.PersonProfileDto;
+import kz.uco.tsadv.service.EmployeeService;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
@@ -21,7 +23,10 @@ public class PortalFeedbackQuestionsChangedListener {
     @Inject
     protected DataManager dataManager;
     @Inject
+    protected EmployeeService employeeService;
+    @Inject
     protected NotificationService notificationService;
+
     protected SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");
 
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
@@ -30,7 +35,11 @@ public class PortalFeedbackQuestionsChangedListener {
             Id<PortalFeedbackQuestions, UUID> entityId = event.getEntityId();
             PortalFeedbackQuestions portalFeedbackQuestions = dataManager.load(entityId).view("portalFeedbackQuestions.edit").one();
 
+            PersonProfileDto dto = employeeService.personProfile(portalFeedbackQuestions.getUser().getPersonGroup().getId());
+
             Map<String, Object> map = new HashMap<>();
+            map.put("mail", dto.getEmail());
+            map.put("phone", dto.getPhone());
             map.put("createTs", format.format(portalFeedbackQuestions.getCreateTs()));
             map.put("fullName", portalFeedbackQuestions.getUser().getPersonGroup().getFullName());
             map.put("userName", portalFeedbackQuestions.getUser().getLogin());
